a9a00054b40cf27696dfe60c217ceb273530a8a5
Cinzia <cinzia.malangone@gmail.com>
2021-06-23 19:08:29 +0200

    Literature path

--- terraform/modules/clickhouse/scripts/clickhouse_startup.sh
+++ terraform/modules/clickhouse/scripts/clickhouse_startup.sh
@@ -324,12 +324,12 @@ clickhouse-client --multiline --multiquery < aotf.sql
 echo "Association on the fly table done"
 
 clickhouse-client --multiline --multiquery < w2v_log.sql
-gsutil -m cat gs://${GS_ETL_DATASET}/literature/vectors/part\* | clickhouse-client -h localhost --query="insert into ot.ml_w2v_log format JSONEachRow "
+gsutil -m cat gs://${GS_ETL_DATASET}/literature/parquet/vectors/part\* | clickhouse-client -h localhost --query="insert into ot.ml_w2v_log format JSONEachRow "
 clickhouse-client --multiline --multiquery < w2v.sql
 echo "Literature vectors done"
 
 clickhouse-client --multiline --multiquery < literature_log.sql
-gsutil -m cat gs://${GS_ETL_DATASET}/literature/literatureIndex/part\* | clickhouse-client -h localhost --query="insert into ot.literature_log format JSONEachRow "
+gsutil -m cat gs://${GS_ETL_DATASET}/literature/parquet/literatureIndex/part\* | clickhouse-client -h localhost --query="insert into ot.literature_log format JSONEachRow "
 clickhouse-client --multiline --multiquery < literature.sql
 echo "Literature table done"
 
