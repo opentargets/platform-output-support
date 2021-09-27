e1d857e05311fe92ffd6037461b35cacd74d8237
Jarrod Baker <jarrod.bakeriii@gmail.com>
2021-09-27 09:48:01 +0100

    Add MP data to ES

--- terraform/modules/posvm/scripts/load_all_data.sh
+++ terraform/modules/posvm/scripts/load_all_data.sh
@@ -49,6 +49,12 @@ export ID="id"
 export INDEX_SETTINGS=$PREFIX_DATA/index_settings.json
 ./load_json_esbulk.sh
 
+# Mouse phenotypes
+export INPUT=$PREFIX_DATA"mp"
+export INDEX_NAME="mp"
+export INDEX_SETTINGS=$PREFIX_DATA/index_settings.json
+./load_json_esbulk.sh
+
 # /faers/json/raw/*
 export INDEX_NAME="openfda_faers"
 export INPUT=$PREFIX_DATA"faers"
