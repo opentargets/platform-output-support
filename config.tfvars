2611a18053bb5fd6792bee585c406873e929edbf
Cinzia <cinzia.malangone@gmail.com>
2021-11-02 10:16:39 +0000

    New ES

--- config.tfvars
+++ config.tfvars
@@ -1,15 +1,15 @@
 # Variables for Sync data
 # Attempt eg 21.09.1
-release_id_dev                            = "21.09.5"
+release_id_dev                            = "21.09.P"
 # Production eg 21.09
 release_id_prod                           = "21.09"
-gs_sync_from                              = "open-targets-pre-data-releases/21.09.5"
+gs_sync_from                              = "open-targets-pre-data-releases/21.09.partners"
 
 # Variable for creating IMAGES : ElasticSearch and Clickhouse
-config_direct_json                         = "open-targets-pre-data-releases/21.09.5"
-config_gs_etl                              = "open-targets-pre-data-releases/21.09.5/output"
-config_script_name                         = "pos"
-config_release_name                        = "platform21-09-5"
+config_direct_json                         = "open-targets-pre-data-releases/21.09.partners"
+config_gs_etl                              = "open-targets-pre-data-releases/21.09.partners/output"
+config_script_name                         = "posp"
+config_release_name                        = "platform21-09-P"
 
 # Project dev info
 config_project_id                           = "open-targets-eu-dev"
@@ -19,7 +19,7 @@ config_gcp_default_zone                     = "europe-west1-d"
 config_vm_elastic_search_vcpus              = "4"
 config_vm_elastic_search_mem                = "32768"
 config_vm_elastic_search_boot_disk_size     = 350
-config_vm_elastic_search_version            = "7.9.0"
+config_vm_elastic_search_version            = "7.15.1"
 
 config_vm_clickhouse_vcpus              = "4"
 config_vm_clickhouse_mem                = "26624"
--- deploy_bq/create_bq.sh
+++ deploy_bq/create_bq.sh
@@ -2,54 +2,83 @@
 
 version_tag=${RELEASE_ID}
 project_id=${PROJECT_ID}
-underscore_version_tag=${version_tag//[\.]/_}
 path_prefix="gs://${GS_ETL_DATASET}/etl/parquet"
 
-datasets=$(gsutil -m cat "${path_prefix}/metadata/**/part*" | jq -r '.id' )
+if [ ${project_id} == "open-targets-prod" ]; then
+    echo "Production - no suffix - allAuthenticatedUsers ON"
+    suffix=""
+else
+    echo "Dev - suffix dev - allAuthenticatedUsers OFF"
+    suffix="_dev2"
+fi
 
-bq --project_id=${project_id} --location='eu' mk platform_${underscore_version_tag}
+bq --project_id=${project_id} --location='eu' rm -f -r platform${suffix}
+bq --project_id=${project_id} --location='eu' mk platform${suffix}
+
+echo ${version_tag} > platform_sync_v.csv
+bq --project_id=${project_id} --location='eu' mk platform${suffix}.ot_release
+bq --project_id=${project_id} --dataset_id=platform${suffix} --location='eu' load platform${suffix}.ot_release platform_sync_v.csv release:string
+
+
+
+datasets=$(curl -X GET https://raw.githubusercontent.com/opentargets/platform-app/main/src/pages/DownloadsPage/dataset-mappings.json | jq -r '.[] | select( .include_in_bq == true) | .id ')
 
 for ds in $datasets
 do
   echo $ds
-  bq --project_id=${project_id} --location='eu' mk platform_${underscore_version_tag}.${ds}
 
-  if [[ $ds = evidence* ]]
-  then
-    bq --project_id=${project_id} --dataset_id=platform_${underscore_version_tag} --location='eu' load --autodetect --source_format=PARQUET \
-      --hive_partitioning_mode=STRINGS \
-      --hive_partitioning_source_uri_prefix="${path_prefix}/${ds}/" ${ds}  "${path_prefix}/${ds}/sourceId*"
+  gsutil list "gs://${GS_ETL_DATASET}/**/parquet/"$ds"/_SUCCESS" 2> /dev/null || true
+  status=$?
+  echo ${status}
+
+  if [[ $status == 0 ]]; then
+    bq --project_id=${project_id} --location='eu' mk platform${suffix}.${ds}
+    if [[ $ds = evidence* ]]
+    then
+      bq --project_id=${project_id} --dataset_id=platform${suffix} --location='eu' load --autodetect --source_format=PARQUET \
+        --hive_partitioning_mode=STRINGS \
+        --hive_partitioning_source_uri_prefix="${path_prefix}/${ds}/" ${ds}  "${path_prefix}/${ds}/sourceId*"
+    else
+      bq --project_id=${project_id} --dataset_id=platform${suffix} --location='eu' load --autodetect --source_format=PARQUET ${ds} \
+        "${path_prefix}/${ds}/part*"
+    fi
   else
-    bq --project_id=${project_id} --dataset_id=platform_${underscore_version_tag} --location='eu' load --autodetect --source_format=PARQUET ${ds} \
-      "${path_prefix}/${ds}/part*"
+     echo "=== ERROR " $ds ": table does not exist"
   fi
 done
 
-bq --project_id=${project_id} --location='eu' mk platform_${underscore_version_tag}.faersRaw
-bq --project_id=${project_id} --dataset_id=platform_${underscore_version_tag} --location='eu' load --autodetect --source_format=PARQUET faersRaw \
- "gs://${GS_ETL_DATASET}/faers/parquet/raw/part*"
+#datasets=$(gsutil -m cat "${path_prefix}/metadata/**/part*" | jq -r '.id' )
+#for ds in $datasets
+#do
+#  echo $ds
+#  bq --project_id=${project_id} --location='eu' mk platform.${ds}
 
-bq --project_id=${project_id} --location='eu' mk platform_${underscore_version_tag}.faersSignificant
-bq --project_id=${project_id} --dataset_id=platform_${underscore_version_tag} --location='eu' load --autodetect --source_format=PARQUET faersSignificant \
-  "gs://${GS_ETL_DATASET}/faers/parquet/significant/part*"
-
-# The mouse phenotypes data does not come from the ETL; it is provided directly from the data team via PIS.
-bq --project_id=${project_id} --location='eu' mk platform_${underscore_version_tag}.mousePhenotypes
-bq --project_id=${project_id} --dataset_id=platform_${underscore_version_tag} --location='eu' load --autodetect --source_format=NEWLINE_DELIMITED_JSON mousePhenotypes \
-  "gs://${GS_ETL_DATASET}/${version_tag}/input/mousePhenotypes/*.json"
-
+#  if [[ $ds = evidence* ]]
+#  then
+#    bq --project_id=${project_id} --dataset_id=platform --location='eu' load --autodetect --source_format=PARQUET \
+#      --hive_partitioning_mode=STRINGS \
+#      --hive_partitioning_source_uri_prefix="${path_prefix}/${ds}/" ${ds}  "${path_prefix}/${ds}/sourceId*"
+#  else
+#    bq --project_id=${project_id} --dataset_id=platform --location='eu' load --autodetect --source_format=PARQUET ${ds} \
+#      "${path_prefix}/${ds}/part*"
+#  fi
+#done
 
 # Adding allUserAuth roles
-bq show --format=prettyjson ${project_id}:platform_${underscore_version_tag} > platform_${underscore_version_tag}_schema.json
-jq --argjson groupInfo '{"role":"roles/bigquery.metadataViewer", "specialGroup": "allAuthenticatedUsers"}' '.access += [$groupInfo]' platform_${underscore_version_tag}_schema.json > platform_${underscore_version_tag}_meta.json
-jq --argjson groupInfo '{"role":"READER", "specialGroup": "allAuthenticatedUsers"}' '.access += [$groupInfo]' platform_${underscore_version_tag}_meta.json > platform_${underscore_version_tag}_new_schema.json
+if [ ${project_id} == "open-targets-prod" ]; then
+  bq show --format=prettyjson ${project_id}:platform > platform_schema.json
+  jq --argjson groupInfo '{"role":"roles/bigquery.metadataViewer", "specialGroup": "allAuthenticatedUsers"}' '.access += [$groupInfo]' platform_schema.json > platform_meta.json
+  jq --argjson groupInfo '{"role":"READER", "specialGroup": "allAuthenticatedUsers"}' '.access += [$groupInfo]' platform_meta.json > platform_new_schema.json
 
-bq update --source platform_${underscore_version_tag}_new_schema.json ${project_id}:platform_${underscore_version_tag}
+  bq update --source platform_new_schema.json ${project_id}:platform
 
-rm platform_${underscore_version_tag}_schema.json
-rm platform_${underscore_version_tag}_meta.json
-rm platform_${underscore_version_tag}_new_schema.json
-
+  rm platform_sync_v.csv
+  rm platform_schema.json
+  rm platform_meta.json
+  rm platform_new_schema.json
+else
+    echo "The dataset must not be visible: allAuthenticatedUsers OFF"
+fi
 
 # Debug: view the new roles for the BiqQuery dataset
 #bq show --format=prettyjson ${project_id}:platform_${underscore_version_tag}
