c077dbec2612c0d62f04e5914fe36ca997971fe3
Cinzia <cinzia.malangone@gmail.com>
2021-12-01 07:58:12 +0000

    fix otar

--- terraform_create_images/modules/posvm/scripts/load_data.sh
+++ terraform_create_images/modules/posvm/scripts/load_data.sh
@@ -26,13 +26,13 @@ mkdir -p /tmp/data/faers/
 echo "Copy from GS to local HD"
 gsutil -m cp -r gs://${GS_ETL_DATASET}/etl/json/* /tmp/data/
 
-#gsutil -m cp -r gs://${GS_ETL_DATASET}/otar_projects/* /tmp/data/otar_projects
 gsutil -m cp -r gs://${GS_ETL_DATASET}/etl/json/fda/significantAdverseDrugReactions/* /tmp/data/faers/
 #gsutil -m cp -r gs://${GS_ETL_DATASET}/so/* /tmp/data/so
 gsutil list -r gs://${GS_DIRECT_FILES} | grep so.json | xargs -t -I % gsutil cp %  /tmp/data/so
 gsutil list -r gs://${GS_DIRECT_FILES} | grep diseases_efo | xargs -t -I % gsutil cp %  gs://${GS_DIRECT_FILES}/webapp/ontology/efo_json/
 #TODO: remove in the next release. Used to test the command output
-gsutil list -r gs://${GS_DIRECT_FILES} | grep diseases_efo | xargs -t -I % gsutil cp %  /tmp/data/
+gsutil list -r gs://${GS_DIRECT_FILES} | grep diseases_efo | xargs -t  -I % gsutil cp %  /tmp/data/
+gsutil list -r gs://${GS_DIRECT_FILES} | grep otar_projects.tsv | xargs -t -I % gsutil cp %  /tmp/data/otar_projects/
 
 sudo mkdir -p /tmp
 cd /tmp
