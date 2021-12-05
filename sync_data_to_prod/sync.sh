ac514d9a4bcccb4f748c84e032035b48666b1913
Cinzia <cinzia.malangone@gmail.com>
2021-12-05 09:29:38 +0000

    rsync

--- sync_data_to_prod/sync.sh
+++ sync_data_to_prod/sync.sh
@@ -6,7 +6,7 @@ ssh noah-login-02 "sudo -u otftpuser mkdir /nfs/ftp/private/otftpuser/upload/pla
 ssh noah-login-02 "tmux new-session -d -s gscopytoftp"
 ssh noah-login-02 "tmux send-keys 'cd /nfs/ftp/private/otftpuser/upload/platform/${RELEASE_ID_PROD}; CLOUDSDK_PYTHON=/nfs/production/opentargets/anaconda3/bin/python /nfs/production/opentargets/google-cloud-sdk/bin/gsutil rsync -r -x ^input/fda-inputs/* gs://${GS_SYNC_FROM}/ . '  C-m"
 ssh noah-login-02 "tmux send-keys 'sudo -u otftpuser mkdir /nfs/ftp/pub/databases/opentargets/platform/${RELEASE_ID_PROD}; '  C-m"
-ssh noah-login-02 "tmux send-keys 'sudo -u otftpuser cp -R /nfs/ftp/private/otftpuser/upload/platform/${RELEASE_ID_PROD}/* /nfs/ftp/pub/databases/opentargets/platform/${RELEASE_ID_PROD}'  C-m"
+ssh noah-login-02 "tmux send-keys 'sudo -u otftpuser rsync -rv /nfs/ftp/private/otftpuser/upload/platform/${RELEASE_ID_PROD}/* /nfs/ftp/pub/databases/opentargets/platform/${RELEASE_ID_PROD}'  C-m"
 ssh noah-login-02 "tmux send-keys 'cd /nfs/ftp/pub/databases/opentargets/platform/; sudo -u otftpuser chmod -R 755 ${RELEASE_ID_PROD}'  C-m"
 ssh noah-login-02 "tmux send-keys 'cd /ebi/ftp/pub/databases/opentargets/platform'  C-m"
 ssh noah-login-02 "tmux send-keys 'sudo -u otftpuser ln -nsf ${RELEASE_ID_PROD} latest'  C-m"
