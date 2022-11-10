96140f00d38badf6a3f6b3793cafa99aede3d451
Jarrod Baker <jarrod.bakeriii@gmail.com>
2022-11-10 12:37:33 +0000

    Update path to metadata files.
    The default metadata location has been changed in the ETL. This reflects
    that change.
    
    Relates to opentargets/platform#2780

--- terraform_create_images/modules/posvm/scripts/load_data.sh
+++ terraform_create_images/modules/posvm/scripts/load_data.sh
@@ -14,7 +14,8 @@ echo "CH: "${CLICKHOUSE_URI}", ES:"${ELASTICSEARCH_URI}", GS: "${GS_ETL_DATASET}
 echo "Partner instance: "${IS_PARTNER_INSTANCE}
 
 #Query the elasticsearch - log purpose
-curl -X GET  ${ELASTICSEARCH_URI}:9200/_cat/indices; echo
+curl -X GET ${ELASTICSEARCH_URI}:9200/_cat/indices
+echo
 
 mkdir -p /tmp/data
 mkdir -p /tmp/data/so
@@ -29,13 +30,13 @@ gsutil -m cp -r gs://${GS_ETL_DATASET}/etl/json/* /tmp/data/
 
 gsutil -m cp -r gs://${GS_ETL_DATASET}/etl/json/fda/significantAdverseDrugReactions/* /tmp/data/faers/
 #gsutil -m cp -r gs://${GS_ETL_DATASET}/so/* /tmp/data/so
-gsutil list -r gs://${GS_DIRECT_FILES} | grep so.json | xargs -t -I % gsutil cp %  /tmp/data/so
-gsutil list -r gs://${GS_DIRECT_FILES} | grep diseases_efo | xargs -t -I % gsutil cp %  gs://${GS_DIRECT_FILES}/webapp/ontology/efo_json/
-echo "---> Create the downloads information file object, metadata collection from 'gs://${GS_ETL_DATASET}/etl/*/metadata/**/*.json' to 'gs://${GS_DIRECT_FILES}/webapp/downloads.json'"
-gsutil cat 'gs://${GS_ETL_DATASET}/etl/*/metadata/**/*.json' > /tmp/data/webapp/downloads.json
+gsutil list -r gs://${GS_DIRECT_FILES} | grep so.json | xargs -t -I % gsutil cp % /tmp/data/so
+gsutil list -r gs://${GS_DIRECT_FILES} | grep diseases_efo | xargs -t -I % gsutil cp % gs://${GS_DIRECT_FILES}/webapp/ontology/efo_json/
+echo "---> Create the downloads information file object, metadata collection from 'gs://${GS_ETL_DATASET}/etl/metadata/**/*.json' to 'gs://${GS_DIRECT_FILES}/webapp/downloads.json'"
+gsutil cat 'gs://${GS_ETL_DATASET}/etl/metadata/**/*.json' >/tmp/data/webapp/downloads.json
 gsutil cp /tmp/data/webapp/downloads.json gs://${GS_DIRECT_FILES}/webapp/downloads.json
 #TODO: remove in the next release. Used to test the command output
-gsutil list -r gs://${GS_DIRECT_FILES} | grep diseases_efo | xargs -t  -I % gsutil cp %  /tmp/data/
+gsutil list -r gs://${GS_DIRECT_FILES} | grep diseases_efo | xargs -t -I % gsutil cp % /tmp/data/
 gsutil -m cp -r gs://${GS_ETL_DATASET}/etl/json/otar_projects/* /tmp/data/otar_projects/
 
 sudo mkdir -p /tmp
@@ -50,7 +51,6 @@ sudo chmod 555 load_json_esbulk.sh
 #sudo wget https://raw.githubusercontent.com/opentargets/platform-output-support/main/terraform/modules/posvm/scripts/load_json.sh
 #sudo chmod 555 load_json.sh
 
-
 sudo wget -O /tmp/data/index_settings.json https://raw.githubusercontent.com/opentargets/platform-etl-backend/master/elasticsearch/index_settings.json
 sudo wget -O /tmp/data/index_settings_search_known_drugs.json https://raw.githubusercontent.com/opentargets/platform-etl-backend/master/elasticsearch/index_settings_search_known_drugs.json
 sudo wget -O /tmp/data/index_settings_search.json https://raw.githubusercontent.com/opentargets/platform-etl-backend/master/elasticsearch/index_settings_search.json
@@ -60,16 +60,14 @@ export PREFIX_DATA=/tmp/data/
 echo "starting the insertion of data ... Elasticsearch."
 time ./load_all_data.sh
 
-
 POLL=1
 echo "POLL="$POLL
-while [ $POLL != "0" ]
-do
+while [ $POLL != "0" ]; do
   sleep 30
   #allow non zero exit codes since that is what we are checking for
   set +e
   #pipeline script will put a tag on the instance here it checks for this tag
-  gcloud --project ${PROJECT_ID} compute instances list --filter='tags:startup-done' > instance_tmp.txt
+  gcloud --project ${PROJECT_ID} compute instances list --filter='tags:startup-done' >instance_tmp.txt
   cat instance_tmp.txt
 
   # Check if clickhouse is done with the insertion of the data
@@ -85,12 +83,12 @@ done
 gcloud compute --project=${PROJECT_ID} instances stop ${ELASTICSEARCH_URI} --zone ${GC_ZONE}
 
 # stop Clickhouse
-gcloud compute --project=${PROJECT_ID} instances stop ${CLICKHOUSE_URI}	--zone ${GC_ZONE}
+gcloud compute --project=${PROJECT_ID} instances stop ${CLICKHOUSE_URI} --zone ${GC_ZONE}
 
-NOW=`date +'%y%m%d-%H%M%S'`
+NOW=$(date +'%y%m%d-%H%M%S')
 echo $NOW
 #create image from elasticsearch machine
-gcloud compute --project=${PROJECT_ID}  images create ${IMAGE_PREFIX}-$NOW-es  --source-disk ${ELASTICSEARCH_URI}  --family ot-es7     --source-disk-zone ${GC_ZONE}
+gcloud compute --project=${PROJECT_ID} images create ${IMAGE_PREFIX}-$NOW-es --source-disk ${ELASTICSEARCH_URI} --family ot-es7 --source-disk-zone ${GC_ZONE}
 
 #create image from clickhouse image
-gcloud compute --project=${PROJECT_ID}  images create ${IMAGE_PREFIX}-$NOW-ch  --source-disk ${CLICKHOUSE_URI}  --family ot-ch     --source-disk-zone ${GC_ZONE}
+gcloud compute --project=${PROJECT_ID} images create ${IMAGE_PREFIX}-$NOW-ch --source-disk ${CLICKHOUSE_URI} --family ot-ch --source-disk-zone ${GC_ZONE}
