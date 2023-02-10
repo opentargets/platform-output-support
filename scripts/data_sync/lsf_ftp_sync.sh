0115df14f70961790305c895a8c578771791cff3
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-02-10 14:40:47 +0000

    removal of staging area and refactoring of google cloud access

--- scripts/data_sync/lsf_ftp_sync.sh
+++ scripts/data_sync/lsf_ftp_sync.sh
@@ -25,7 +25,6 @@
 export job_name="${LSB_JOBNAME}-${LSB_BATCH_JID}"
 export path_private_base='/nfs/ftp/private/otftpuser'
 export path_private_base_ftp_upload="${path_private_base}/opentargets_ebi_ftp_upload"
-export path_private_staging_folder="${path_private_base_ftp_upload}/${RELEASE_ID_PROD}"
 export path_ebi_ftp_base='/nfs/ftp/public/databases/opentargets/platform'
 export path_ebi_ftp_destination="${path_ebi_ftp_base}/${RELEASE_ID_PROD}"
 export path_ebi_ftp_destination_latest="${path_ebi_ftp_base}/latest"
@@ -63,7 +62,6 @@ function print_summary {
     echo -e "\t- Release Number                     : ${RELEASE_ID_PROD}"
     echo -e "\t- Job Name                           : ${job_name}"
     echo -e "\t- PATH Private base                  : ${path_private_base}"
-    echo -e "\t- PATH Private staging folder        : ${path_private_staging_folder}"
     echo -e "\t- PATH EBI FTP base destination      : ${path_ebi_ftp_base}"
     echo -e "\t- PATH EBI FTP destination folder    : ${path_ebi_ftp_destination}"
     echo -e "\t- PATH EBI FTP destination latest    : ${path_ebi_ftp_destination_latest}"
@@ -92,8 +90,6 @@ function make_dirs {
     sudo -u otftpuser -- bash -c "mkdir ${path_lsf_job_logs} && chmod 770 ${path_lsf_job_logs}"
     log_body "MKDIR" "Check/Create ${path_ebi_ftp_destination}"
     sudo -u otftpuser -- bash -c "mkdir ${path_ebi_ftp_destination} && chmod 775 ${path_ebi_ftp_destination}"
-    log_body "MKDIR" "Check/Create ${path_private_staging_folder}"
-    sudo -u otftpuser -- bash -c "mkdir ${path_private_staging_folder} && chmod 770 ${path_private_staging_folder}"
 }
 
 # GCP functions
@@ -109,37 +105,30 @@ function deactivate_service_account {
 }
 
 function pull_data_from_gcp {
-    log_heading "GCP" "Pulling data from GCP"
-    log_heading "GCP" "Copy source data from '${path_data_source}' ---> to ---> '${path_private_staging_folder}'"
-    #singularity exec --bind /nfs/ftp:/nfs/ftp docker://google/cloud-sdk:latest gsutil -m -u open-targets-prod rsync -r -x ^input/fda-inputs/* ${path_data_source} ${path_private_staging_folder}/
-    log_heading "PERMISSIONS" "Adjusting file tree permissions at '${path_private_staging_folder}'"
-    #find ${path_private_staging_folder} -type d -exec chmod 775 \{} \;
-    #find ${path_private_staging_folder} -type f -exec chmod 644 \{} \;
+    log_heading "GCP" "Pulling data from GCP, '${path_data_source}' ---> to ---> '${path_ebi_ftp_destination}"
+    singularity exec --bind /nfs/ftp:/nfs/ftp docker://google/cloud-sdk:latest gsutil -m rsync -r -x ^input/fda-inputs/* ${path_data_source} ${path_ebi_ftp_destination}/
+    log_heading "PERMISSIONS" "Adjusting file tree permissions at '${path_ebi_ftp_destination}'"
+    find ${path_ebi_ftp_destination} -type d -exec chmod 775 {} \;
+    find ${path_ebi_ftp_destination} -type f -exec chmod 644 {} \;
     log_heading "GCP" "Done pulling data from GCP"
 }
 
 # Helper functions
 function compute_checksums {
     log_heading "CHECKSUM" "Compute SHA1 checksum for all the files in this release"
-    log_heading "DATA" "Compute SHA1 checksum for all the files in this release"
-    #current_dir=`pwd`
-    #cd ${path_private_staging_folder}
-    #find . -type f -exec sha1sum \{} \; > ${filename_release_checksum}
-    # TODO - This part needs a different approach
-    #log_heading "DATA" "Add the data integrity information back to the source bucket"
-    #CLOUDSDK_PYTHON=/nfs/production/opentargets/anaconda3/bin/python /nfs/production/opentargets/google-cloud-sdk/bin/gsutil cp ${filename_release_checksum} ${path_data_source}${filename_release_checksum}
-    #cd ${current_dir}
-}
-
-function promote_data_from_staging_to_ftp {
-    log_heading "RSYNC" "Sync data from '${path_private_staging_folder}' ---> to ---> '${path_ebi_ftp_destination}'"
-    #rsync -vah --stats --delete ${path_private_staging_folder}/ ${path_ebi_ftp_destination}/
+    current_dir=`pwd`
+    cd ${path_ebi_ftp_destination}
+    find . -type f -exec sha1sum \{} \; > ${filename_release_checksum}
+    log_heading "DATA" "Add the data integrity information back to the source bucket"
+    singularity exec --bind /nfs/ftp:/nfs/ftp docker://google/cloud-sdk:latest gsutil cp ${filename_release_checksum} ${path_data_source}${filename_release_checksum}
+    cd ${current_dir}
+    log_heading "CHECKSUM" "Done computing SHA1 checksum for all the files in this release"
 }
 
 function ftp_update_latest_symlink {
     log_heading "FTP" "Update latest symlink"
     log_heading "LATEST" "Update 'latest' link at '${path_ebi_ftp_destination_latest}' to point to '${path_ebi_ftp_destination}'"
-    #ln -nsf $( basename ${path_ebi_ftp_destination} ) ${path_ebi_ftp_destination_latest}
+    ln -nsf $( basename ${path_ebi_ftp_destination} ) ${path_ebi_ftp_destination_latest}
 }
 
 # Bootstrap
@@ -147,7 +136,7 @@ function bootstrap {
     log_heading "BOOTSTRAP" "Bootstrapping"
     activate_service_account
     log_heading "FILESYSTEM" "Preparing destination folders"
-    #make_dirs
+    make_dirs
     log_heading "BOOTSTRAP" "Done"
 }
 
@@ -156,7 +145,7 @@ function cleanup {
     log_heading "CLEAN" "Cleaning up"
     deactivate_service_account
     log_body "CLEAN" "Remove operations folder at '${PATH_OPS_ROOT_FOLDER}'"
-    #rm -rf ${PATH_OPS_ROOT_FOLDER}
+    rm -rf ${PATH_OPS_ROOT_FOLDER}
     log_heading "CLEAN" "Done"
 }
 
@@ -171,6 +160,5 @@ pull_data_from_gcp
 compute_checksums
 promote_data_from_staging_to_ftp
 ftp_update_latest_symlink
-#log_heading "SYNC" "Start a sync of the FTP data from HX staging area to the OY and PG London storages?"
 cleanup
 log_heading "JOB" "END OF JOB ${job_name}"
