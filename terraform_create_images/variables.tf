94bd03206e08d9c76fb7dc8fa479d7bf55aa78fa
remo87 <remo87@gmail.com>
2023-03-23 12:01:34 +0000

    Load priorisation data and git repo branch

--- terraform_create_images/variables.tf
+++ terraform_create_images/variables.tf
@@ -2,130 +2,137 @@
 
 variable "config_script_name" {
   description = "Open Targets Platform script name, not related to any configuration parameter."
-  type = string
+  type        = string
 }
 
 variable "config_release_name" {
   description = "Open Targets Platform release name, parameter for the images"
-  type = string
+  type        = string
 }
 
 variable "config_enable_graphQL" {
   description = "OpenTargets release with graphQL"
-  type = bool
-  default = true
+  type        = bool
+  default     = true
 }
 
 variable "config_gcp_default_region" {
   description = "Default region when not specified in the module"
-  type = string
+  type        = string
 }
 
 variable "config_gcp_default_zone" {
   description = "Default zone when not specified in the module"
-  type = string
+  type        = string
 }
 
 variable "config_project_id" {
   description = "Default project to use when not specified in the module"
-  type = string
+  type        = string
 }
 
 // --- ETL info --- //
 variable "is_partner_instance" {
   description = "Is partners instance? By default false"
-  default = false
+  default     = false
 }
 
 variable "config_gs_etl" {
   description = "Output of the ETL [root]. Eg. open-targets-data-releases/21.04/output"
-  type = string
+  type        = string
 }
 
 variable "config_direct_json" {
   description = "External JSon to laod. Used for SO. Eg. open-targets-data-releases/21.09/input"
-  type = string
+  type        = string
 }
 
 // --- Elastic Search Configuration --- //
 variable "config_vm_elastic_boot_image" {
   description = "Boot image configuration for the deployed Elastic Search Instances"
-  type = string
-  default = "projects/cos-cloud/global/images/family/cos-stable"
+  type        = string
+  default     = "projects/cos-cloud/global/images/family/cos-stable"
 }
 
 
 variable "config_vm_elastic_search_vcpus" {
   description = "CPU count configuration for the deployed Elastic Search Instances"
-  type = number
+  type        = number
 }
 
 variable "config_vm_elastic_search_mem" {
   description = "RAM configuration for the deployed Elastic Search Instances"
-  type = number
+  type        = number
 }
 
 variable "config_vm_elastic_search_version" {
   description = "Elastic search version to deploy"
-  type = string
+  type        = string
 }
 
 variable "config_vm_elastic_search_boot_disk_size" {
   description = "Boot disk size to use for the deployed Elastic Search Instances"
-  type = string
+  type        = string
 }
 
 // --- Clickhouse Configuration --- //
 
 variable "config_vm_clickhouse_boot_image" {
   description = "Boot image configuration for the deployed clickhouse Instances"
-  type = string
-  default = "debian-10"
+  type        = string
+  default     = "debian-10"
 }
 
 variable "config_vm_clickhouse_vcpus" {
   description = "CPU count configuration for the deployed clickhouse Instances"
-  type = number
+  type        = number
 }
 
 variable "config_vm_clickhouse_mem" {
   description = "RAM configuration for the deployed clickhouse Instances"
-  type = number
+  type        = number
 }
 
 variable "config_vm_clickhouse_boot_disk_size" {
   description = "Boot disk size to use for the deployed clickhouse Instances"
-  type = string
+  type        = string
 }
 
 // --- GraphQL VM Configuration. Deploy appEngine. If empty doesn't deploy the appEngine --- //
 variable "config_api_image_version" {
   description = "API Docker image version to use in deployment"
-  type = string
-  default = ""
+  type        = string
+  default     = ""
 }
 
 variable "config_appengine_name" {
   description = "AppEngine dev. promote vs no-promote"
-  type = string
-  default = "pos-api"
+  type        = string
+  default     = "pos-api"
 }
 
 // --- POS VM Configuration --- //
 variable "config_vm_pos_boot_image" {
   description = "Boot image configuration for POS VM"
-  type = string
-  default = "debian-10"
+  type        = string
+  default     = "debian-10"
 }
 
 variable "config_vm_pos_boot_disk_size" {
   description = "POS VM boot disk size, default '500GB'"
-  type = string
-  default = 500
+  type        = string
+  default     = 500
 }
 
 variable "config_vm_pos_machine_type" {
   description = "Machine type for POS vm"
-  type = string
-  default = "debian-10"
+  type        = string
+  default     = "debian-10"
 }
+
+variable "git_branch" {
+  description = "Git branch to aquire SQL scripts from: useful for developing on feature branches."
+  type        = string
+  default     = "main"
+}
+
