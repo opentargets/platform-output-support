fe52acf6581e490f11cf634bda2e7547bb4be573
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-04-19 16:31:13 +0100

    remove posvm module call from main

--- terraform_create_images/main.tf
+++ terraform_create_images/main.tf
@@ -27,25 +27,3 @@ provider "google-beta" {
   region  = var.config_gcp_default_region
   zone    = var.config_gcp_default_zone
 }
-
-// --- Modules --- //
-module "backend_pos_vm" {
-  module_wide_prefix_scope = "${var.config_script_name}-vm"
-  source                   = "./modules/posvm"
-
-  project_id = var.config_project_id
-
-  depends_on = [module.backend_elastic_search, module.backend_clickhouse]
-
-  // Region and zone
-  vm_default_zone       = var.config_gcp_default_zone
-  vm_pos_boot_image     = var.config_vm_pos_boot_image
-  vm_pos_boot_disk_size = var.config_vm_pos_boot_disk_size
-  vm_pos_machine_type   = var.config_vm_pos_machine_type
-  gs_etl                = var.config_gs_etl
-  is_partner_instance   = var.is_partner_instance
-  config_direct_json    = var.config_direct_json
-  #  vm_elasticsearch_uri  = module.backend_elastic_search.elasticsearch_vm_name
-  #  vm_clickhouse_uri     = module.backend_clickhouse.clickhouse_vm_name
-  release_name = var.config_release_name
-}
