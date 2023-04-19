fe0a877b7a16356d742e47621fa65c1b53a6bf76
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-04-19 15:35:14 +0100

    include disk images names in the output from POS

--- terraform_create_images/outputs.tf
+++ terraform_create_images/outputs.tf
@@ -10,5 +10,11 @@ output "pos_support_vm_name" {
   value = module.backend_pos_vm.pos_support_vm_name
 }
 
-// TODO Clickhouse disk image name
-// TODO ElasticSearch disk image name
+// Clickhouse disk image name
+output "disk_image_name_clickhouse" {
+ value = local.image_name_clickhouse 
+}
+// ElasticSearch disk image name
+output "disk_image_name_elastic_search" {
+ value = local.image_name_elastic_search 
+}
