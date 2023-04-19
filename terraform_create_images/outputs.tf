d9a9cfa4ef0eadeca38842f9ee3197d4054df7da
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-04-19 15:36:23 +0100

    remove unused outputs

--- terraform_create_images/outputs.tf
+++ terraform_create_images/outputs.tf
@@ -1,11 +1,4 @@
-output "elasticsearch_hostname" {
-  value = module.backend_elastic_search.*
-}
-
-output "clickhouse_hostname" {
-  value = module.backend_clickhouse.*
-}
-
+// POS VM name
 output "pos_support_vm_name" {
   value = module.backend_pos_vm.pos_support_vm_name
 }
