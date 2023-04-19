4b843223e6884966f6826f7f9034c3032f29e48d
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-04-19 15:34:09 +0100

    CH and ES disk images names

--- terraform_create_images/locals.tf
+++ terraform_create_images/locals.tf
@@ -1,5 +1,10 @@
 locals {
   // Loging to the remote host, details
   remote_user_name = "provisioner"
-  // TODO - Disk Images to be created
+  // Time Stamp to be used in the image name
+  image_timestamp = formatdate("YYYYMMDD-hhmm", timestamp())
+  // Elastic Search disk image name
+  image_name_elastic_search = "${var.config_release_name}-${local.image_timestamp}-es"
+  // Clickhouse disk image name
+  image_name_clickhouse = "${var.config_release_name}-${local.image_timestamp}-ch"
 }
--- terraform_create_images/main.tf
+++ terraform_create_images/main.tf
@@ -81,7 +81,7 @@ module "backend_pos_vm" {
   gs_etl                = var.config_gs_etl
   is_partner_instance   = var.is_partner_instance
   config_direct_json    = var.config_direct_json
-  vm_elasticsearch_uri  = module.backend_elastic_search.elasticsearch_vm_name
-  vm_clickhouse_uri     = module.backend_clickhouse.clickhouse_vm_name
+#  vm_elasticsearch_uri  = module.backend_elastic_search.elasticsearch_vm_name
+#  vm_clickhouse_uri     = module.backend_clickhouse.clickhouse_vm_name
   release_name          = var.config_release_name
 }
