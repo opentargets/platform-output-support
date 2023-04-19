56c3e1deb57439b1be8ffd22ed01b9127b3d8fcf
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-04-19 15:36:49 +0100

    remove unused modules

--- terraform_create_images/main.tf
+++ terraform_create_images/main.tf
@@ -28,43 +28,7 @@ provider "google-beta" {
   zone = var.config_gcp_default_zone
 }
 
-// --- Elastic Search Backend --- //
-# module "backend_elastic_search" {
-#   source                   = "./modules/elasticsearch"
-#   project_id               = var.config_project_id
-#   module_wide_prefix_scope = "${var.config_script_name}-es"
-#   // Elastic Search configuration
-#   vm_elastic_search_version = var.config_vm_elastic_search_version
-#   vm_elastic_search_vcpus   = var.config_vm_elastic_search_vcpus
-#   // Memory size in MiB
-#   vm_elastic_search_mem = var.config_vm_elastic_search_mem
-
-#   // Region and zone
-#   vm_default_region               = var.config_gcp_default_region
-#   vm_default_zone                 = var.config_gcp_default_zone
-#   vm_elastic_boot_image           = var.config_vm_elastic_boot_image
-#   vm_elasticsearch_boot_disk_size = var.config_vm_elastic_search_boot_disk_size
-# }
-
-# module "backend_clickhouse" {
-#   source = "./modules/clickhouse"
-
-#   module_wide_prefix_scope = "${var.config_script_name}-ch"
-#   // Elastic Search configuration
-#   vm_clickhouse_vcpus = var.config_vm_clickhouse_vcpus
-#   // Memory size in MiB
-#   vm_clickhouse_mem = var.config_vm_clickhouse_mem
-#   gs_etl            = var.config_gs_etl
-
-#   // Region and zone
-#   project_id                   = var.config_project_id
-#   vm_default_region            = var.config_gcp_default_region
-#   vm_default_zone              = var.config_gcp_default_zone
-#   vm_clickhouse_boot_image     = var.config_vm_clickhouse_boot_image
-#   vm_clickhouse_boot_disk_size = var.config_vm_clickhouse_boot_disk_size
-# }
-
-
+// --- Modules --- //
 module "backend_pos_vm" {
   module_wide_prefix_scope = "${var.config_script_name}-vm"
   source                   = "./modules/posvm"
