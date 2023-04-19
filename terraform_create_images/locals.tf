99c5db7fa674425c90d7b3ba6351086f7585c416
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-04-19 15:52:54 +0100

    posvm machine name

--- terraform_create_images/locals.tf
+++ terraform_create_images/locals.tf
@@ -1,8 +1,13 @@
 locals {
+  // --- POS VM Configuration --- //
   // Loging to the remote host, details
   posvm_remote_user_name = "provisioner"
   // IAM role to be used to create the VM
   posvm_roles   = ["roles/compute.admin", "roles/logging.viewer","roles/compute.instanceAdmin","roles/storage.objectViewer","roles/storage.admin"]
+  // VM name
+  posvm_name_prefix    = "${var.config_release_name}-posvm"
+  
+  // --- Disk Images Configuration --- //
   // Time Stamp to be used in the image name
   image_timestamp = formatdate("YYYYMMDD-hhmm", timestamp())
   // Elastic Search disk image name
