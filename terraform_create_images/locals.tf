0f0b4c771835eb1886083ba960d0bec392637580
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-04-21 10:27:01 +0100

    data load process configuration section

--- terraform_create_images/locals.tf
+++ terraform_create_images/locals.tf
@@ -6,12 +6,19 @@ locals {
   posvm_roles = ["roles/compute.admin", "roles/logging.viewer", "roles/compute.instanceAdmin", "roles/storage.objectViewer", "roles/storage.admin"]
   // VM name
   posvm_name_prefix = "${var.config_release_name}-posvm"
+  
+  // --- POS VM data load process configuration --- //
   // Clickhouse data disk device name
   data_disk_device_name_clickhouse = "clickhouse-data-disk"
   // ElasticSearch data disk device name
   data_disk_device_name_elastic_search = "elastic-search-data-disk"
   // Google Device Disk prefix
   gcp_device_disk_prefix = "/dev/disk/by-id/google-"
+  // Clickhouse path to data mount point
+  path_mount_data_clickhouse = "/mnt/clickhouse"
+  // ElasticSearch path to data mount point
+  path_mount_data_elastic_search = "/mnt/elasticsearch"
+  // --- [END] POS VM data load process configuration [END] --- //
 
   // --- Disk Images Configuration --- //
   // Time Stamp to be used in the image name
