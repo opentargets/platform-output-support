68c3785faea42586891c52f0eacebde6152c964b
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-04-21 10:59:31 +0100

    postprocessing pipeline entry point

---
+++ terraform_create_images/scripts/launch_pos.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# This script launches the postprocessing pipeline
+
+# Bootstrapping environment
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# Create a temporary folder for the script
+TMPDIR=$(mktemp -d)
+
+# Load common configuration script
+source ${SCRIPTDIR}/config.sh
+
+
+
+
+
+
+
+
+
+
+# Main
+log "Mount data disks for Clickhouse and Elastic Search"
+mount_disk $${gcp_device_disk_clickhouse} $${mount_point_data_clickhouse}
+mount_disk $${gcp_device_disk_elasticsearch} $${mount_point_data_elasticsearch}
