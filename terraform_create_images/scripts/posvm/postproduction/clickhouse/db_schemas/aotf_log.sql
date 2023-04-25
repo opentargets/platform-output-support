38f0e196957757741301b9bf64afb6ec118a07b0
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-04-26 00:09:09 +0100

    database initialization script for AOTF

---
+++ terraform_create_images/scripts/posvm/postproduction/clickhouse/db_schemas/aotf_log.sql
@@ -0,0 +1,11 @@
+create database if not exists ot;
+create table if not exists ot.associations_otf_log(
+    row_id String,
+    disease_id String,
+    target_id String,
+    disease_data Nullable(String),
+    target_data Nullable(String),
+    datasource_id String,
+    datatype_id String,
+    row_score Float64
+) engine = Log;
