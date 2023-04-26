542cf243f13dfe04940b2dd9624922b21d2e82f7
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-04-26 13:06:56 +0100

    env vars refactoring

--- terraform_create_images/scripts/posvm/postproduction/clickhouse/run.sh
+++ terraform_create_images/scripts/posvm/postproduction/clickhouse/run.sh
@@ -7,40 +7,34 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Local configuration
 source ${SCRIPTDIR}/config.sh
 
-# DEBUG - exit here
-exit 0
-
-
-
-
 # Helper functions
 # Prepare Clickhouse Storage Volume
 function prepare_clickhouse_storage_volume() {
   log "[START] Preparing Clickhouse Storage Volume"
   # Create Clickhouse Storage Volume folders
-  mkdir -p ${ch_vol_path_clickhouse_config}
-  mkdir -p ${ch_vol_path_clickhouse_users}
-  mkdir -p ${ch_vol_path_clickhouse_data}
+  mkdir -p ${pos_ch_vol_path_clickhouse_config}
+  mkdir -p ${pos_ch_vol_path_clickhouse_users}
+  mkdir -p ${pos_ch_vol_path_clickhouse_data}
   # Copy Clickhouse configuration files
-  cp ${pos_ch_path_config}/* ${ch_vol_path_clickhouse_config}
+  cp ${pos_ch_path_config}/* ${pos_ch_vol_path_clickhouse_config}
   # Copy Clickhouse users configuration files
-  cp ${pos_ch_path_users}/* ${ch_vol_path_clickhouse_users}
+  cp ${pos_ch_path_users}/* ${pos_ch_vol_path_clickhouse_users}
   log "[DONE] Preparing Clickhouse Storage Volume"
 }
 
 # Run clickhouse via Docker 
 function run_clickhouse() {
-  log "[START] Running clickhouse via Docker, using image ${CLICKHOUSE_DOCKER_IMAGE}:${CLICKHOUSE_DOCKER_IMAGE_VERSION}"
+  log "[START] Running clickhouse via Docker, using image ${pos_ch_docker_image}"
   docker run --rm -d \
-    --name ${ch_docker_container_name} \
+    --name ${pos_ch_docker_container_name} \
     -p 9000:9000 \
     -p 8123:8123 \
-    -v ${ch_vol_path_clickhouse_config}:/etc/clickhouse-server/config.d \
-    -v ${ch_vol_path_clickhouse_users}:/etc/clickhouse-server/users.d \
-    -v ${ch_vol_path_clickhouse_data}:/var/lib/clickhouse \
-    -v ${path_logs_clickhouse}:/var/log/clickhouse-server \
+    -v ${pos_ch_vol_path_clickhouse_config}:/etc/clickhouse-server/config.d \
+    -v ${pos_ch_vol_path_clickhouse_users}:/etc/clickhouse-server/users.d \
+    -v ${pos_ch_vol_path_clickhouse_data}:/var/lib/clickhouse \
+    -v ${pos_path_logs_clickhouse}:/var/log/clickhouse-server \
     -v ${pos_ch_path_schemas}:/docker-entrypoint-initdb.d \
-    -v ${pos_ch_path_sql_scripts_postdataload}:${ch_path_sql_scripts_postdataload} \
+    -v ${pos_ch_path_sql_scripts_postdataload}:${pos_ch_vol_path_sql_scripts_postdataload} \
     --ulimit nofile=262144:262144 \
     ${ch_docker_image}
 }
@@ -48,7 +42,7 @@ function run_clickhouse() {
 # Wait for Clickhouse to be ready
 function wait_for_clickhouse() {
   log "[INFO] Waiting for Clickhouse to be ready"
-  while ! docker exec ${ch_docker_container_name} clickhouse-client --query "SELECT 1" &> /dev/null; do
+  while ! docker exec ${pos_ch_docker_container_name} clickhouse-client --query "SELECT 1" &> /dev/null; do
     sleep 1
   done
   log "[INFO] Clickhouse is ready"
@@ -60,11 +54,11 @@ function load_release_data() {
   # Wait for Clickhouse to be ready
   wait_for_clickhouse
   # Load release data
-  log "[INFO] Loading release data from '${DATA_RELEASE_PATH_SOURCE_ROOT}' into Clickhouse"
-  for table in "$(!ch_data_release_sources[@])"; do
-    export path_source="${DATA_RELEASE_PATH_ETL_JSON}/${ch_data_release_sources[$table]}"
+  log "[INFO] Loading release data from '${pos_data_release_path_source_root}' into Clickhouse"
+  for table in "$(!pos_ch_data_release_sources[@])"; do
+    export path_source="${pos_data_release_path_etl_json}/${pos_ch_data_release_sources[$table]}"
     log "[INFO] Loading release data from '${path_source}' into Clickhouse table '${table}'"
-    gsutil -m cat ${path_source} | docker exec -i ${ch_docker_container_name} clickhouse-client --query="insert into ${table} format JSONEachRow "
+    gsutil -m cat ${path_source} | docker exec -i ${pos_ch_docker_container_name} clickhouse-client --query="insert into ${table} format JSONEachRow "
   done
   log "[DONE] Loading release data into Clickhouse"
 }
