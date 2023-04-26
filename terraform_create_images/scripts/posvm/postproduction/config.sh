ad27cd4d38084a49b594f502040e05f1896ffaf2
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-04-26 11:58:57 +0100

    convert configuration variables into env vars

--- terraform_create_images/scripts/posvm/postproduction/config.sh
+++ terraform_create_images/scripts/posvm/postproduction/config.sh
@@ -6,52 +6,52 @@ SCRIPTDIR="$( cd "$( dirname "$${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Environment variables
 # Remapping of values rendered by Terraform into environment variables (there must way another way to do this, that it is not so ugly)
-pos_project_id=${POS_PROJECT_ID}
-pos_gc_zone=${POS_GC_ZONE}
-pos_gs_etl_dataset=${POS_GS_ETL_DATASET}
-pos_is_partner_instance=${POS_IS_PARTNER_INSTANCE}
-pos_gs_direct_files=${POS_GS_DIRECT_FILES}
-pos_gcp_device_disk_prefix=${POS_GCP_DEVICE_DISK_PREFIX}
-pos_data_disk_device_name_ch=${POS_DATA_DISK_DEVICE_NAME_CH}
-pos_data_disk_device_name_es=${POS_DATA_DISK_DEVICE_NAME_ES}
-pos_disk_image_name_ch=${POS_DISK_IMAGE_NAME_CH}
-pos_disk_image_name_es=${POS_DISK_IMAGE_NAME_ES}
-pos_path_mount_data_clickhouse=${POS_PATH_MOUNT_DATA_CLICKHOUSE}
-pos_path_mount_data_elasticsearch=${POS_PATH_MOUNT_DATA_ELASTICSEARCH}
-pos_path_postprocessing_scripts_clickhouse=${POS_PATH_POSTPROCESSING_SCRIPTS_CLICKHOUSE}
-pos_path_postprocessing_scripts_elastic_search=${POS_PATH_POSTPROCESSING_SCRIPTS_ELASTIC_SEARCH}
-pos_path_postprocessing_scripts_entry_point_clickhouse=${POS_PATH_POSTPROCESSING_SCRIPTS_ENTRY_POINT_CLICKHOUSE}
-pos_path_postprocessing_scripts_entry_point_elastic_search=${POS_PATH_POSTPROCESSING_SCRIPTS_ENTRY_POINT_ELASTIC_SEARCH}
-pos_data_release_sekeleton_path_output_root=${POS_DATA_RELEASE_SEKELETON_PATH_OUTPUT_ROOT}
-pos_data_release_sekeleton_path_etl_root=${POS_DATA_RELEASE_SEKELETON_PATH_ETL_ROOT}
-pos_data_release_sekeleton_path_etl_json_root=${POS_DATA_RELEASE_SEKELETON_PATH_ETL_JSON_ROOT}
-pos_data_release_sekeleton_path_etl_parquet_root=${POS_DATA_RELEASE_SEKELETON_PATH_ETL_PARQUET_ROOT}
-pos_data_release_path_source_root=${POS_DATA_RELEASE_PATH_SOURCE_ROOT}
-pos_data_release_path_etl_json=${POS_DATA_RELEASE_PATH_ETL_JSON}
-pos_data_release_path_etl_parquet=${POS_DATA_RELEASE_PATH_ETL_PARQUET}
-pos_clickhouse_docker_image=${POS_CLICKHOUSE_DOCKER_IMAGE}
-pos_clickhouse_docker_image_version=${POS_CLICKHOUSE_DOCKER_IMAGE_VERSION}
-pos_pos_repo_branch=${POS_POS_REPO_BRANCH}
-pos_flag_postprocessing_scripts_ready=${POS_FLAG_POSTPROCESSING_SCRIPTS_READY}
-pos_path_postprocessing_root=${POS_PATH_POSTPROCESSING_ROOT}
-pos_path_postprocessing_scripts=${POS_PATH_POSTPROCESSING_SCRIPTS}
-pos_filename_postprocessing_scripts_entry_point=${POS_FILENAME_POSTPROCESSING_SCRIPTS_ENTRY_POINT}
-pos_clickhouse_uri=${POS_CLICKHOUSE_URI}
-pos_elasticsearch_uri=${POS_ELASTICSEARCH_URI}
-pos_image_prefix=${POS_IMAGE_PREFIX}
+export pos_project_id=${POS_PROJECT_ID}
+export pos_gc_zone=${POS_GC_ZONE}
+export pos_gs_etl_dataset=${POS_GS_ETL_DATASET}
+export pos_is_partner_instance=${POS_IS_PARTNER_INSTANCE}
+export pos_gs_direct_files=${POS_GS_DIRECT_FILES}
+export pos_gcp_device_disk_prefix=${POS_GCP_DEVICE_DISK_PREFIX}
+export pos_data_disk_device_name_ch=${POS_DATA_DISK_DEVICE_NAME_CH}
+export pos_data_disk_device_name_es=${POS_DATA_DISK_DEVICE_NAME_ES}
+export pos_disk_image_name_ch=${POS_DISK_IMAGE_NAME_CH}
+export pos_disk_image_name_es=${POS_DISK_IMAGE_NAME_ES}
+export pos_path_mount_data_clickhouse=${POS_PATH_MOUNT_DATA_CLICKHOUSE}
+export pos_path_mount_data_elasticsearch=${POS_PATH_MOUNT_DATA_ELASTICSEARCH}
+export pos_path_postprocessing_scripts_clickhouse=${POS_PATH_POSTPROCESSING_SCRIPTS_CLICKHOUSE}
+export pos_path_postprocessing_scripts_elastic_search=${POS_PATH_POSTPROCESSING_SCRIPTS_ELASTIC_SEARCH}
+export pos_path_postprocessing_scripts_entry_point_clickhouse=${POS_PATH_POSTPROCESSING_SCRIPTS_ENTRY_POINT_CLICKHOUSE}
+export pos_path_postprocessing_scripts_entry_point_elastic_search=${POS_PATH_POSTPROCESSING_SCRIPTS_ENTRY_POINT_ELASTIC_SEARCH}
+export pos_data_release_sekeleton_path_output_root=${POS_DATA_RELEASE_SEKELETON_PATH_OUTPUT_ROOT}
+export pos_data_release_sekeleton_path_etl_root=${POS_DATA_RELEASE_SEKELETON_PATH_ETL_ROOT}
+export pos_data_release_sekeleton_path_etl_json_root=${POS_DATA_RELEASE_SEKELETON_PATH_ETL_JSON_ROOT}
+export pos_data_release_sekeleton_path_etl_parquet_root=${POS_DATA_RELEASE_SEKELETON_PATH_ETL_PARQUET_ROOT}
+export pos_data_release_path_source_root=${POS_DATA_RELEASE_PATH_SOURCE_ROOT}
+export pos_data_release_path_etl_json=${POS_DATA_RELEASE_PATH_ETL_JSON}
+export pos_data_release_path_etl_parquet=${POS_DATA_RELEASE_PATH_ETL_PARQUET}
+export pos_clickhouse_docker_image=${POS_CLICKHOUSE_DOCKER_IMAGE}
+export pos_clickhouse_docker_image_version=${POS_CLICKHOUSE_DOCKER_IMAGE_VERSION}
+export pos_pos_repo_branch=${POS_POS_REPO_BRANCH}
+export pos_flag_postprocessing_scripts_ready=${POS_FLAG_POSTPROCESSING_SCRIPTS_READY}
+export pos_path_postprocessing_root=${POS_PATH_POSTPROCESSING_ROOT}
+export pos_path_postprocessing_scripts=${POS_PATH_POSTPROCESSING_SCRIPTS}
+export pos_filename_postprocessing_scripts_entry_point=${POS_FILENAME_POSTPROCESSING_SCRIPTS_ENTRY_POINT}
+export pos_clickhouse_uri=${POS_CLICKHOUSE_URI}
+export pos_elasticsearch_uri=${POS_ELASTICSEARCH_URI}
+export pos_image_prefix=${POS_IMAGE_PREFIX}
 
 # Newly defined
-pos_gcp_device_disk_clickhouse="${POS_GCP_DEVICE_DISK_PREFIX}${POS_DATA_DISK_DEVICE_NAME_CH}"
-pos_gcp_device_disk_elasticsearch="${POS_GCP_DEVICE_DISK_PREFIX}${POS_DATA_DISK_DEVICE_NAME_ES}"
+export pos_gcp_device_disk_clickhouse="${POS_GCP_DEVICE_DISK_PREFIX}${POS_DATA_DISK_DEVICE_NAME_CH}"
+export pos_gcp_device_disk_elasticsearch="${POS_GCP_DEVICE_DISK_PREFIX}${POS_DATA_DISK_DEVICE_NAME_ES}"
 # Data mount points
-pos_mount_point_data_clickhouse="${POS_PATH_MOUNT_DATA_CLICKHOUSE}"
-pos_mount_point_data_elasticsearch="${POS_PATH_MOUNT_DATA_ELASTICSEARCH}"
+export pos_mount_point_data_clickhouse="${POS_PATH_MOUNT_DATA_CLICKHOUSE}"
+export pos_mount_point_data_elasticsearch="${POS_PATH_MOUNT_DATA_ELASTICSEARCH}"
 # Log folders
-pos_path_logs_postprocessing="${POS_PATH_POSTPROCESSING_ROOT}/logs"
-pos_path_logs_clickhouse="$${pos_path_logs_postprocessing}/clickhouse"
-pos_path_logs_elastic_search="$${pos_path_logs_postprocessing}/elasticsearch"
+export pos_path_logs_postprocessing="${POS_PATH_POSTPROCESSING_ROOT}/logs"
+export pos_path_logs_clickhouse="$${pos_path_logs_postprocessing}/clickhouse"
+export pos_path_logs_elastic_search="$${pos_path_logs_postprocessing}/elasticsearch"
 # List of folders that need to exist for the postprocessing scripts to run
-pos_list_folders_postprocessing="$${pos_path_logs_postprocessing} $${pos_path_logs_clickhouse} $${pos_path_logs_elastic_search}"
+export pos_list_folders_postprocessing="$${pos_path_logs_postprocessing} $${pos_path_logs_clickhouse} $${pos_path_logs_elastic_search}"
 
 # Helper functions
 # Logging helper function
@@ -62,8 +62,8 @@ function log() {
 # Print a summary with the running environment (all environment variables starting with POS_)
 function env_summary() {
   log "[GLOBAL] Environment summary:"
-  for var in $(env | grep pos_); do
-    log "  - $${!var} = $${var}"
+  for var in $(compgen -e | grep pos_); do
+    log "  - $${var} = $${!var}"
   done
 }
 
