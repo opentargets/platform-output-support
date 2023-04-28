4a345204c936488fd218203fd54840229f1d98aa
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-04-28 11:57:10 +0100

    local config for ES postprocessing pipeline

---
+++ terraform_create_images/scripts/posvm/postproduction/elasticsearch/config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Elastic Search postprocessing pipeline local configuration
+
+# Bootstrapping environment
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Load global configuration
+source ${SCRIPTDIR}/../config.sh
+
