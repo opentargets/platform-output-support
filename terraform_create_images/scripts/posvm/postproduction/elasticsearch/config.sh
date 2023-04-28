1f7f1075926331279210ba806ebbcb9e4ca1b8e4
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-04-28 12:13:37 +0100

    data folder for elastic search files

--- terraform_create_images/scripts/posvm/postproduction/elasticsearch/config.sh
+++ terraform_create_images/scripts/posvm/postproduction/elasticsearch/config.sh
@@ -8,3 +8,7 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Load global configuration
 source ${SCRIPTDIR}/../config.sh
 
+# Local configuration
+# Elastic Search volume paths
+# Path to the folder with Elastic Search data files
+export pos_es_vol_path_data="${pos_mount_point_data_elasticsearch}/data"
