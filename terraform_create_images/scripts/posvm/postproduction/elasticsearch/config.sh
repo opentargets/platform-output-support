560b2c8e4665136cd32be98c886583f86d22b5e9
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-04-28 12:21:12 +0100

    ES runtime config and environment summary

--- terraform_create_images/scripts/posvm/postproduction/elasticsearch/config.sh
+++ terraform_create_images/scripts/posvm/postproduction/elasticsearch/config.sh
@@ -12,3 +12,25 @@ source ${SCRIPTDIR}/../config.sh
 # Elastic Search volume paths
 # Path to the folder with Elastic Search data files
 export pos_es_vol_path_data="${pos_mount_point_data_elasticsearch}/data"
+
+# Elastic Search runtime configuration
+export pos_es_docker_container_name="otp-es"
+export pos_es_docker_image="${pos_elasticsearch_docker_image}:${pos_elasticsearch_docker_image_version}"
+
+# Print summary of the environment by looping through all those variables that start with "pos_es_"
+function env_summary() {
+    echo "[ELASTICSEARCH] Postprocessing pipeline environment summary:"
+    for var in "${!pos_es_@}"; do
+        var_declaration=$(declare -p $var 2>/dev/null)
+        if [[ $var_declaration == *"declare -A"* ]]; then
+            # The variable is an associative array
+            local -n array_reference=${var}
+        for key in "${!array_reference[@]}"; do
+                echo "    ${var}[$key]=${array_reference[$key]}"
+            done
+        else
+            # The variable is a primitive variable
+            echo "    $var=${!var}"
+        fi
+    done
+}
