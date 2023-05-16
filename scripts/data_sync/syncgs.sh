0fa1c34128857c3bfe942317fabcd7d22e288c78
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-05-16 11:59:32 +0100

    prevent data sync process from moving data into our production bucket if PPP

--- scripts/data_sync/syncgs.sh
+++ scripts/data_sync/syncgs.sh
@@ -5,7 +5,12 @@ path_data_source="gs://${DATA_LOCATION_SOURCE}/"
 # TODO - Change the way the command is run
 # TODO - Add a check to see if the release is already there
 # TODO - Skipped failed matches from literature
-# TODO - DO NOT RUN IF THIS IS A PPP INSTANCE
+# Check if it's partner instance
+if [ "${IS_PARTNER_INSTANCE}" = true ]; then
+    echo "This is a partner instance, skipping the sync process"
+    exit 0
+fi
+
 echo "=== Start copying..."
 cmd=`gsutil -m rsync -r -x '^input/fda-inputs/*' $path_data_source gs://open-targets-data-releases/${RELEASE_ID_PROD}/`
 echo "=== Copying done."
