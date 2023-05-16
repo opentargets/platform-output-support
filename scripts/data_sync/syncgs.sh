080f30794b5d7a9085b9ca10afb53fb681e17102
Manuel Bernal Llinares <mbdebian@gmail.com>
2023-05-16 09:18:40 +0100

    TODO on preventing data sync to public bucket if profile is PPP

--- scripts/data_sync/syncgs.sh
+++ scripts/data_sync/syncgs.sh
@@ -5,6 +5,7 @@ path_data_source="gs://${DATA_LOCATION_SOURCE}/"
 # TODO - Change the way the command is run
 # TODO - Add a check to see if the release is already there
 # TODO - Skipped failed matches from literature
+# TODO - DO NOT RUN IF THIS IS A PPP INSTANCE
 echo "=== Start copying..."
 cmd=`gsutil -m rsync -r -x '^input/fda-inputs/*' $path_data_source gs://open-targets-data-releases/${RELEASE_ID_PROD}/`
 echo "=== Copying done."
