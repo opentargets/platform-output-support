1e9427dd1dd295e73f6e260758ab4baeab3b7179
James Hayhurst <hayhurst.jd@gmail.com>
2024-03-08 16:09:37 +0000

    lsf to slurm

--- scripts/data_sync/launch_ebi_ftp_sync.sh
+++ scripts/data_sync/launch_ebi_ftp_sync.sh
@@ -8,15 +8,15 @@ set -e
 
 # Environment
 export BASEDIR=$(dirname $0)
-export LSF_JOB_SCRIPT="${BASEDIR}/lsf_ftp_sync.sh"
+export SLURM_JOB_SCRIPT="${BASEDIR}/ftp_sync.sh"
 export DST_PATH_RELATIVE_OPS="ot-ops"
 export DST_PATH_RELATIVE_CREDENTIALS="${DST_PATH_RELATIVE_OPS}/credentials"
 export DST_GCP_CREDENTIALS_FILENAME="gcp_credentials.json"
 export DST_PATH_RELATIVE_CREDENTIALS_FILE="${DST_PATH_RELATIVE_CREDENTIALS}/${DST_GCP_CREDENTIALS_FILENAME}"
-[ -z "${LSF_USER}" ] && export LSF_USER=`whoami`
-[ -z "${LSF_QUEUE}" ] && export LSF_QUEUE="datamover"
+[ -z "${SLURM_USER}" ] && export SLURM_USER=`whoami`
+[ -z "${SLURM_QUEUE}" ] && export SLURM_QUEUE="datamover"
 [ -z "${EBI_LOGIN_GATE}" ] && export EBI_LOGIN_GATE="ligate.ebi.ac.uk"
-[ -z "${EBI_LOGIN_NODE}" ] && export EBI_LOGIN_NODE="codon-login-02"
+[ -z "${EBI_LOGIN_NODE}" ] && export EBI_LOGIN_NODE="codon-slurm-login"
 [ -z "${RELEASE_ID_PROD}" ] && export RELEASE_ID_PROD='dev.default_release_id'
 [ -z "${DATA_LOCATION_SOURCE}" ] && export DATA_LOCATION_SOURCE="open-targets-pre-data-releases/${RELEASE_ID_PROD}"
 [ -z "${IS_PARTNER_INSTANCE}" ] && export IS_PARTNER_INSTANCE=true
@@ -44,11 +44,11 @@ log_error() {
 
 # Summary of the environment
 log_heading "INFO" "Launching EBI FTP sync job"
-log_body "INFO" "                                LSF User: ${LSF_USER}"
-log_body "INFO" "                               LSF Queue: ${LSF_QUEUE}"
+log_body "INFO" "                              SLURM User: ${SLURM_USER}"
+log_body "INFO" "                             SLURM Queue: ${SLURM_QUEUE}"
 log_body "INFO" "                          EBI Login Gate: ${EBI_LOGIN_GATE}"
 log_body "INFO" "                          EBI Login Node: ${EBI_LOGIN_NODE}"
-log_body "INFO" "                          LSF Job Script: ${LSF_JOB_SCRIPT}"
+log_body "INFO" "                        SLURM Job Script: ${SLURM_JOB_SCRIPT}"
 log_body "INFO" "                    Data Source Location: ${DATA_LOCATION_SOURCE}"
 log_body "INFO" "                 Release ID (Production): ${RELEASE_ID_PROD}"
 log_body "INFO" "                    GCP Credentials file: ${PATH_GCS_CREDENTIALS_FILE}"
@@ -63,7 +63,7 @@ fi
 
 # Prepare the credentials file
 log_body "INFO" "Preparing ops folder, \$HOME/${DST_PATH_RELATIVE_OPS} and credentials file, \$HOME/${DST_PATH_RELATIVE_CREDENTIALS_FILE}"
-cat ${PATH_GCS_CREDENTIALS_FILE} | ssh -o proxycommand="ssh -p 2244 ${LSF_USER}@${EBI_LOGIN_GATE} proxy %h" ${LSF_USER}@${EBI_LOGIN_NODE} "/bin/bash -c 'source /etc/bashrc; mkdir -p \$HOME/${DST_PATH_RELATIVE_CREDENTIALS}; chmod -R 750 \$HOME/${DST_PATH_RELATIVE_OPS}; cat - > \$HOME/${DST_PATH_RELATIVE_CREDENTIALS_FILE}; chmod 640 \$HOME/${DST_PATH_RELATIVE_CREDENTIALS_FILE}'"
+cat ${PATH_GCS_CREDENTIALS_FILE} | ssh -o proxycommand="ssh -p 2244 ${SLURM_USER}@${EBI_LOGIN_GATE} proxy %h" ${SLURM_USER}@${EBI_LOGIN_NODE} "/bin/bash -c 'source /etc/bashrc; mkdir -p \$HOME/${DST_PATH_RELATIVE_CREDENTIALS}; chmod -R 750 \$HOME/${DST_PATH_RELATIVE_OPS}; cat - > \$HOME/${DST_PATH_RELATIVE_CREDENTIALS_FILE}; chmod 640 \$HOME/${DST_PATH_RELATIVE_CREDENTIALS_FILE}'"
 # Launch the job
-log_body "INFO" "Launching the job on the LSF cluster"
-cat ${LSF_JOB_SCRIPT} | ssh -o proxycommand="ssh -p 2244 ${LSF_USER}@${EBI_LOGIN_GATE} proxy %h" ${LSF_USER}@${EBI_LOGIN_NODE} "/bin/bash -c 'source /etc/bashrc; export PATH_OPS_ROOT_FOLDER=\$HOME/${DST_PATH_RELATIVE_OPS}; export PATH_OPS_CREDENTIALS=\$HOME/${DST_PATH_RELATIVE_CREDENTIALS_FILE}; export RELEASE_ID_PROD=${RELEASE_ID_PROD}; export DATA_LOCATION_SOURCE=${DATA_LOCATION_SOURCE}; export LSF_QUEUE=${LSF_QUEUE}; bsub -q ${LSF_QUEUE}'"
+log_body "INFO" "Launching the job on the SLURM cluster"
+cat ${SLURM_JOB_SCRIPT} | ssh -o proxycommand="ssh -p 2244 ${SLURM_USER}@${EBI_LOGIN_GATE} proxy %h" ${SLURM_USER}@${EBI_LOGIN_NODE} "/bin/bash -c 'source /etc/bashrc; export PATH_OPS_ROOT_FOLDER=\$HOME/${DST_PATH_RELATIVE_OPS}; export PATH_OPS_CREDENTIALS=\$HOME/${DST_PATH_RELATIVE_CREDENTIALS_FILE}; export RELEASE_ID_PROD=${RELEASE_ID_PROD}; export DATA_LOCATION_SOURCE=${DATA_LOCATION_SOURCE}; export SLURM_QUEUE=${SLURM_QUEUE}; sbatch --partition=${SLURM_QUEUE}'"
