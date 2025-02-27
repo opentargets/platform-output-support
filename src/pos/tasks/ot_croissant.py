9176b59f0dae321c11fb68ad83f3fe40c4d5d971
ricardo <remo87@gmail.com>
2025-02-27 10:36:07 +0000

    update config, update file to gcp

--- src/pos/tasks/ot_croissant.py
+++ src/pos/tasks/ot_croissant.py
@@ -1,8 +1,12 @@
 # OT Croissant Task
 
+from pathlib import Path
+from typing import Self
+
 from otter.task.model import Spec, Task, TaskContext
 from otter.util.errors import OtterError
 from otter.task.task_reporter import report
+from otter.storage import get_remote_storage
 
 import json
 from datetime import datetime
@@ -24,7 +28,7 @@ class OtCroissantSpec(Spec):
         - d (str): The path where the parquet outputs are stored. These outputs are going to be used to extract the schema.
         - version (str): The version of the data.
         - date_published (str): The date when the data was published. The date format is YYYY-MM-DD.
-        - output (str): The path where the output is going to be stored. The output is a Croissant file witht the metadata of the pipeline execution.
+        - output (str): Name of the file where the metadata is going to be stored.
     """
 
     ftp_address: str
@@ -43,10 +47,12 @@ class OtCroissant(Task):
     def __init__(self, spec: OtCroissantSpec, context: TaskContext) -> None:
         super().__init__(spec, context)
         self.spec: OtCroissantSpec
+        self.local_path: Path = self.context.config.work_path / self.spec.output
+        if context.config.release_uri:
+            self.remote_uri = f'{context.config.release_uri}/{spec.output}'
 
     @report
-    def run(self) -> None:
-        logger.info("Running OT Croissant task")
+    def run(self) -> Self:
         metadata = PlatformOutputMetadata(
             datasets=[self.spec.d],
             ftp_location= self.spec.ftp_address,
@@ -55,9 +61,20 @@ class OtCroissant(Task):
             date_published=self.spec.date_published,
             data_integrity_hash='sha256'
         )
+        logger.debug(f"Metadata generated: {metadata}")
 
-        with open(self.spec.output, "w") as f:
+        with open(self.local_path, "w") as f:
                 content = metadata.to_json()
                 content = json.dumps(content, indent=2, default=datetime_serializer)
                 f.write(content)
                 f.write("\n")
+                logger.debug(f"Metadata written to {self.local_path}")
+
+         # upload the result to remote storage
+        if self.remote_uri:
+            logger.info(f'Uploading {self.local_path} to {self.remote_uri}')
+            remote_storage = get_remote_storage(self.remote_uri)
+            remote_storage.upload(self.local_path, self.remote_uri)
+            logger.debug('upload successful')
+
+        return self
