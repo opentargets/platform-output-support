c07b3e4212649d6c8c38181219235c3e4097374d
remo87 <remo87@gmail.com>
2025-03-10 10:29:57 +0000

    3658 create downloads file (#59)
    * create tasks
    
    * read files build id and resources
    
    * write jsonl file
    
    * add ot_croissant execution
    
    * remove transformation as is no longer needed
    
    * update config, update file to gcp
    
    * update config
    
    * add reference
    
    * fix remote_uri declaration
    
    * create file if it doesn't exists
    
    * applied pr comments
    
    * add artifact
    
    * move local artifact
    
    * refactor artifact
    
    * fix param in config
    
    * update dataset input to list
    
    * add rsync and update ot_croissant paths
    
    * update release
    
    * update output folders
    
    * update datasetpaths
    
    * update lit model files
    
    * remove paths that brake the generation
    
    * update args descriptions
    
    * update source arg descriptions
    
    * update config with correct release
    
    * update colo
    
    * format tasks
    
    * apply comments from pr 59
    
    * update log messages
    
    * Python config base
    
    * add reference
    
    * reformat files
    
    * update comments and config paths
    
    * enter
    
    ---------
    
    Co-authored-by: Javier Ferrer <jferrer@ebi.ac.uk>

---
+++ src/pos/tasks/ot_croissant.py
@@ -0,0 +1,107 @@
+# OT Croissant Task
+
+import json
+from datetime import datetime
+from pathlib import Path
+from typing import Self
+
+from loguru import logger
+from ot_croissant.crumbs.metadata import PlatformOutputMetadata
+from otter.manifest.model import Artifact
+from otter.storage import get_remote_storage
+from otter.task.model import Spec, Task, TaskContext
+from otter.task.task_reporter import report
+from otter.util.errors import OtterError, ScratchpadError
+
+
+class OtCroissantError(OtterError):
+    """Base class for exceptions in this module."""
+
+
+class OtCroissantSpec(Spec):
+    """Configuration fields for the OT Croissant task.
+
+    This task has the following custom configuration fields:
+        - ftp_address (str): The URL of the ftp where the data \
+          is going to be published.
+        - gcp_address (str): The URL of the google bucket where \
+          the data is going to be published.
+        - dataset_path (Path): The path where the parquet outputs \
+          are stored. These outputs are going to be used to extract \
+          the schema.
+        - date_published (str): The date when the data was published. \
+          The date format is YYYY-MM-DD.
+        - output (str): Path (relative to `work_path` or `release_uri`) \
+          to store the metadata at.
+    """
+
+    ftp_address: str
+    gcp_address: str
+    dataset_paths: list[Path]
+    date_published: str
+    output: str
+
+
+def datetime_serializer(obj):
+    if isinstance(obj, datetime):
+        return obj.isoformat()
+    raise TypeError(f'Type {type(obj)} not serializable')
+
+
+class OtCroissant(Task):
+    def __init__(self, spec: OtCroissantSpec, context: TaskContext) -> None:
+        super().__init__(spec, context)
+        self.spec: OtCroissantSpec
+        self.local_path: Path = self.context.config.work_path / self.spec.output
+        self.remote_uri: str | None = None
+        if context.config.release_uri:
+            self.remote_uri = f'{context.config.release_uri}/{spec.output}'
+
+    @report
+    def run(self) -> Self:
+        release = self.context.scratchpad.sentinel_dict.get('release')
+        if not release:
+            raise ScratchpadError('"release" not found in the scratchpad')
+
+        # Converting the list of paths to a list of strings and prepending the work_path
+        logger.debug(
+            'converting the list of paths to a list of strings and prepending the work_path'
+        )
+        datasets = [
+            str(self.context.config.work_path / p)
+            for p in self.spec.dataset_paths
+        ]
+
+        logger.debug(f'generating metadata for release {release}')
+        metadata = PlatformOutputMetadata(
+            datasets=datasets,
+            ftp_location=self.spec.ftp_address,
+            gcp_location=self.spec.gcp_address,
+            version=release,
+            date_published=self.spec.date_published,
+            data_integrity_hash='sha256',
+        )
+        logger.debug(f'Metadata generated: {metadata}')
+
+        with open(self.local_path, 'w+') as f:
+            logger.debug('writting metadata to localpath')
+            metadata_json = metadata.to_json()
+            json.dump(metadata_json, f, indent=2)
+            f.write('\n')
+            logger.debug(f'metadata written successfully to {self.local_path}')
+
+        # upload the result to remote storage
+        if self.remote_uri:
+            logger.info(f'uploading {self.local_path} to {self.remote_uri}')
+            remote_storage = get_remote_storage(self.remote_uri)
+            remote_storage.upload(self.local_path, self.remote_uri)
+            logger.debug('metadata upload successful')
+
+        # TODO: set all the inputs for artifact. This has to be done after the functionality is implemented in otter
+        self.artifacts = [
+            Artifact(
+                source=f'{self.spec.gcp_address}',
+                destination=self.remote_uri or str(self.local_path),
+            )
+        ]
+        return self
---
+++ src/pos/tasks/sync_bucket.py
@@ -0,0 +1,59 @@
+# Sync bucket
+
+import subprocess
+from pathlib import Path
+from typing import Self
+
+from loguru import logger
+from otter.task.model import Spec, Task, TaskContext
+from otter.task.task_reporter import report
+from otter.util.fs import check_dir
+
+
+class SyncBucketError(Exception):
+    """Base class for exceptions in this module."""
+
+
+class SyncBucketSpec(Spec):
+    """Configuration fields for the sync bucket task.
+
+    This task has the following custom configuration fields:
+        - source (str): The gcloud URL of the parquet file/directory or files.
+        - destination (Path): The path, relative to `work_path` to download the
+            outputs to.
+    """
+
+    source: str
+    destination: Path
+
+
+class SyncBucket(Task):
+    def __init__(self, spec: SyncBucketSpec, context: TaskContext) -> None:
+        super().__init__(spec, context)
+        self.spec: SyncBucketSpec
+        self.context: TaskContext
+
+    @report
+    def run(self) -> Self:
+        destination_folder = self.context.config.work_path.joinpath(
+            self.spec.destination
+        )
+
+        logger.debug(
+            f'checking if {destination_folder} exists. If not, create it'
+        )
+        # Checking if the destination folder exists. If not, create it.
+        check_dir(destination_folder)
+
+        logger.debug(f'syncing {self.spec.source} with {self.spec.destination}')
+        rsync_command = [
+            'gsutil',
+            '-m',
+            'rsync',
+            '-r',
+            self.spec.source,
+            destination_folder,
+        ]
+        subprocess.run(rsync_command, check=True)
+
+        return self
