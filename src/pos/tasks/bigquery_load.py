d411ee07dad6d64012c58167efc4969bcf505c22
James Hayhurst <hayhurst.jd@gmail.com>
2025-03-17 17:22:10 +0000

    add bigquery step
    Update config.yaml
    
    add bigquery
    
    add bigquery dev step
    
    Delete bigquery.py
    
    update datatsets and add docstrings to bigquery.py
    
    fix bq access
    
    update bigquery datasets, enable hive partitioning
    
    update bigquery tables and fix hive partitioning

---
+++ src/pos/tasks/bigquery_load.py
@@ -0,0 +1,58 @@
+# Big query load task
+
+from pathlib import Path
+from typing import Self
+
+from loguru import logger
+from otter.task.model import Spec, Task, TaskContext
+from otter.task.task_reporter import report
+from otter.util.errors import OtterError
+
+from pos.gcp.bigquery import BigQuery
+from pos.utils import get_config
+
+
+class BigqueryLoadError(OtterError):
+    """Base class for exceptions in this module."""
+
+
+class BigqueryLoadSpec(Spec):
+    """Configuration fields for the BigQuery task."""
+
+    dataset_id: str = 'platform_dev'
+    project_id: str = 'open-targets-eu-dev'
+    location: str = 'eu'
+    table: str
+    base_uri: str
+    file_pattern: str = '*.parquet'
+    format: str = 'PARQUET'
+
+
+class BigqueryLoad(Task):
+    def __init__(self, spec: BigqueryLoadSpec, context: TaskContext) -> None:
+        super().__init__(spec, context)
+        self.spec: BigqueryLoadSpec
+        try:
+            self._config = get_config('config/datasets.yaml').bigquery
+            self._table_name = self._config[self.spec.table].index
+            self._input_dir = self._config[self.spec.table].input_dir
+            self._path = f'{self.spec.base_uri}/{self._input_dir}/{self.spec.file_pattern}'
+            self._hive_partition = bool(self._config[self.spec.table].get('hive_partition'))
+        except AttributeError:
+            raise BigqueryLoadError(f'Unable to load config for {self.spec.table}')
+
+    @report
+    def run(self) -> Self:
+        bigquery = BigQuery(project=self.spec.project_id, location=self.spec.location, dataset=self.spec.dataset_id)
+        if self._hive_partition:
+            logger.debug(f'Loading {self._path} into {self._table_name} with hive partition')
+            hive_partition_source = Path(f'{self.spec.base_uri}/{self._input_dir}').parent
+            bigquery.load_from_uri(
+                self._path,
+                self._table_name,
+                self.spec.format,
+                hive_partition_source=hive_partition_source,
+            )
+        else:
+            bigquery.load_from_uri(self._path, self._table_name, self.spec.format)
+        return self
---
+++ src/pos/tasks/bigquery_reset.py
@@ -0,0 +1,53 @@
+# Big query reset task
+
+from typing import Self
+
+from google.cloud.bigquery.schema import SchemaField
+from loguru import logger
+from otter.task.model import Spec, Task, TaskContext
+from otter.task.task_reporter import report
+from otter.util.errors import OtterError
+
+from pos.gcp.bigquery import BigQuery
+
+
+class BigqueryResetError(OtterError):
+    """Base class for exceptions in this module."""
+
+
+class BigqueryResetSpec(Spec):
+    """Configuration fields for the BigQuery task."""
+
+    dataset_id: str = 'platform_dev'
+    project_id: str = 'open-targets-eu-dev'
+    location: str = 'eu'
+    release_id: str
+
+
+class BigqueryReset(Task):
+    RELEASE_META_TABLE = 'ot_release'
+    RELEASE_META_TABLE_SCHEMA = SchemaField('release', 'STRING')
+
+    def __init__(self, spec: BigqueryResetSpec, context: TaskContext) -> None:
+        super().__init__(spec, context)
+        self.spec: BigqueryResetSpec
+        self._release_metadata = [{'release': self.spec.release_id}]
+
+    @report
+    def run(self) -> Self:
+        bigquery = BigQuery(project=self.spec.project_id, location=self.spec.location, dataset=self.spec.dataset_id)
+        bigquery.create_dataset()
+        self._load_release_metadata(bigquery)
+        if self._is_prod():
+            logger.debug('Prod, so making dataset public')
+            bigquery.make_dataset_access_public()
+        return self
+
+    def _load_release_metadata(self, bigquery: BigQuery) -> None:
+        bigquery.create_table(self.RELEASE_META_TABLE)
+        bigquery.load_from_json(
+            self._release_metadata, self.RELEASE_META_TABLE, schema=[self.RELEASE_META_TABLE_SCHEMA]
+        )
+
+    def _is_prod(self) -> bool:
+        return self.spec.project_id == self.context.scratchpad.sentinel_dict.get('bq_prod_project_id')
