5d8bc2cecaa5f451f1cda82ae01e75f57e8f9b6e
James Hayhurst <hayhurst.jd@gmail.com>
2025-04-10 11:41:32 +0100

    added base class for containerized services, updated clickhouse service
    add timeout reset to health check
    
    reorganise services, added run_container helper method

--- src/pos/tasks/open_search_start.py
+++ src/pos/tasks/open_search_start.py
@@ -6,7 +6,7 @@ from otter.task.model import Spec, Task, TaskContext
 from otter.task.task_reporter import report
 from otter.util.errors import OtterError
 
-from pos.opensearch.service import OpenSearchInstanceManager
+from pos.services.opensearch import OpenSearchInstanceManager
 
 
 class OpenSearchStartError(OtterError):
@@ -17,11 +17,10 @@ class OpenSearchStartSpec(Spec):
     """Configuration fields for the start OpenSearch task."""
 
     service_name: str = 'os-pos'
-    host: str = 'localhost'
-    port: str = '9200'
+    # host: str = 'localhost'
+    # port: str = '9200'
     volume_data: str
     volume_logs: str
-    volume_creds: str
     opensearch_java_opts: str
 
 
@@ -33,11 +32,10 @@ class OpenSearchStart(Task):
     @report
     def run(self) -> Self:
         logger.debug(f'Starting OpenSearch instance {self.spec.service_name}')
-        opensearch = OpenSearchInstanceManager(self.spec.service_name, self.spec.host, self.spec.port)
+        opensearch = OpenSearchInstanceManager(self.spec.service_name)
         opensearch.start(
             self.spec.volume_data,
             self.spec.volume_logs,
-            self.spec.volume_creds,
             self.spec.opensearch_java_opts,
         )
         return self
