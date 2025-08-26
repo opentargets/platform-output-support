aeae81b47147901efa82396834565e1fb77634e8
Javier Ferrer <jferrer@ebi.ac.uk>
2025-08-26 09:46:02 +0100

    add baseline expression dataset

---
+++ config/clickhouse/scripts/baseline_expression.sql
@@ -0,0 +1,11 @@
+create table if not exists baseline_expression engine = MergeTree ()
+order by (
+        targetId,
+        tissueBiosampleId,
+        celltypeBiosampleId
+    ) settings allow_nullable_key = 1 as (
+        select *
+        from baseline_expression_log
+    );
+
+drop table baseline_expression_log;
