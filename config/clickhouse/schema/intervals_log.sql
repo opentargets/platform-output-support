7495278c0d5adedbfc3b2e52470530ef7ebd3ca9
James Hayhurst <hayhurst.jd@gmail.com>
2025-08-27 16:39:31 +0100

    intervals schema change

--- config/clickhouse/schema/intervals_log.sql
+++ config/clickhouse/schema/intervals_log.sql
@@ -1,20 +1,49 @@
 create database if not exists ot;
 
-CREATE TABLE if not exists ot.intervals_log 
-(  
-    `chromosome` Enum8('1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', 'X', 'Y'),
-	`start` UInt32,
+CREATE TABLE if not exists ot.intervals_log (
+    `chromosome` Enum8 (
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '10',
+        '11',
+        '12',
+        '13',
+        '14',
+        '15',
+        '16',
+        '17',
+        '18',
+        '19',
+        '20',
+        '21',
+        '22',
+        'X',
+        'Y',
+        'MT'
+    ),
+    `start` UInt32,
     `end` UInt32,
     `geneId` String,
-    `biosampleName` LowCardinality(String),
-    `biosampleId` LowCardinality(String),
-    `intervalType` LowCardinality(String),
-    `distanceToTSS` UInt32,
+    `biosampleName` LowCardinality (String),
+    `biosampleId` LowCardinality (String),
+    `intervalType` LowCardinality (String),
+    `distanceToTss` Int32,
     `score` Float64,
-    `resourceScore` Array(Tuple(name LowCardinality(String), value Float64)),
-    `datasourceId` LowCardinality(String),
+    `resourceScore` Array (
+        Tuple (
+            name LowCardinality (String),
+            value Float64
+        )
+    ),
+    `datasourceId` LowCardinality (String),
     `pmid` String,
     `studyId` String,
-) 
-engine = Log;
+) engine = Log;
 ;
