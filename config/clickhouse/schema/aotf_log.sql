4f7186ea9060aca03f6804c27bf4d638948a6d4e
James Hayhurst <hayhurst.jd@gmail.com>
2025-09-09 10:53:50 +0100

    use lowcardinality types for aotf

--- config/clickhouse/schema/aotf_log.sql
+++ config/clickhouse/schema/aotf_log.sql
@@ -2,11 +2,11 @@ create database if not exists ot;
 
 create table if not exists ot.associations_otf_log (
     row_id String,
-    disease_id String,
-    target_id String,
+    disease_id LowCardinality (String),
+    target_id LowCardinality (String),
     disease_data Nullable (String),
     target_data Nullable (String),
-    datasource_id String,
-    datatype_id String,
+    datasource_id LowCardinality (String),
+    datatype_id LowCardinality (String),
     row_score Float64
 ) engine = Log;
