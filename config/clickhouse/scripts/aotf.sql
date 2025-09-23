907038cdd7dea4e64863855ce9de02430d6a17c8
James Hayhurst <hayhurst.jd@gmail.com>
2025-09-23 15:07:12 +0100

    update aotf sql script

--- config/clickhouse/scripts/aotf.sql
+++ config/clickhouse/scripts/aotf.sql
@@ -1,37 +1,53 @@
-create table if not exists ot.associations_otf_disease engine = MergeTree ()
-order by (A, B, datasource_id) primary key (A) as
-select
+CREATE TABLE IF NOT EXISTS ot.associations_otf_indirect engine = MergeTree PRIMARY KEY row_id
+ORDER BY (row_id) AS
+SELECT
     row_id,
-    A,
-    B,
+    arrayFilter (
+        x -> (x > 0),
+        groupArray (r.row_score)
+    ) AS indirect_scores
+FROM
+    ot.associations_otf_log
+    LEFT JOIN ot.disease_log ON associations_otf_log.disease_id = ot.disease_log.id ARRAY
+    JOIN descendants
+    LEFT JOIN ot.associations_otf_log AS r ON (descendants = r.disease_id)
+    AND (target_id = r.target_id)
+    AND (
+        datasource_id = r.datasource_id
+    )
+GROUP BY
+    row_id;
+
+CREATE TABLE IF NOT EXISTS ot.associations_otf_disease ENGINE = MergeTree PRIMARY KEY A
+ORDER BY (A, B, datasource_id) AS
+SELECT
+    row_id,
+    disease_id AS A,
+    target_id AS B,
     datatype_id,
     datasource_id,
     row_score,
-    A_search,
-    B_search
-from (
-        select
-            row_id, disease_id as A, target_id as B, datatype_id, datasource_id, row_score, lower(disease_data) as A_search, lower(target_data) as B_search
-        from ot.associations_otf_log
-    );
+    lower(disease_data) AS A_search,
+    lower(target_data) AS B_search,
+    indirect_scores
+FROM ot.associations_otf_log
+    LEFT JOIN ot.associations_otf_indirect ON row_id = ot.associations_otf_indirect.row_id;
 
-create table if not exists ot.associations_otf_target engine = MergeTree ()
-order by (A, B, datasource_id) primary key (A) as
-select
+CREATE TABLE IF NOT EXISTS ot.associations_otf_target ENGINE = MergeTree PRIMARY KEY A
+ORDER BY (A, B, datasource_id) AS
+SELECT
     row_id,
-    A,
-    B,
+    target_id AS A,
+    disease_id AS B,
     datatype_id,
     datasource_id,
     row_score,
-    A_search,
-    B_search,
-    ancestors
-from (
-        select
-            row_id, disease_id as B, target_id as A, datatype_id, datasource_id, row_score, lower(disease_data) as B_search, lower(target_data) as A_search, ancestors
-        from ot.associations_otf_log
-            LEFT JOIN ot.disease_log ON ot.associations_otf_log.disease_id = ot.disease_log.id
-    );
+    lower(target_data) AS A_search,
+    lower(disease_data) AS B_search,
+    indirect_scores
+FROM ot.associations_otf_log
+    LEFT JOIN ot.associations_otf_indirect ON row_id = ot.associations_otf_indirect.row_id;
 
-drop table ot.associations_otf_log;
+DROP TABLE ot.associations_otf_log;
+
+DROP TABLE ot.associations_otf_indirect;
