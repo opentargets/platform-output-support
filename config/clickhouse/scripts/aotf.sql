fc1b7489b7da02e90ea70e0724e6b52581d08928
James Hayhurst <hayhurst.jd@gmail.com>
2025-11-17 10:53:30 +0000

    aotf merge with disease for measurement filter

--- config/clickhouse/scripts/aotf.sql
+++ config/clickhouse/scripts/aotf.sql
@@ -25,11 +25,19 @@ select
     datasource_id,
     row_score,
     A_search,
-    B_search
+    B_search,
+    has (
+        therapeuticAreas,
+        'EFO_0001444'
+    ) as is_measurement
 from (
         select
-            row_id, disease_id as B, target_id as A, datatype_id, datasource_id, row_score, lower(disease_data) as B_search, lower(target_data) as A_search
-        from associations_otf_log
+            row_id, disease_id as B, target_id as A, datatype_id, datasource_id, row_score, lower(disease_data) as B_search, lower(target_data) as A_search, therapeuticAreas
+        from
+            associations_otf_log
+            left outer join disease_log on associations_otf_log.disease_id = disease_log.id
     );
 
 drop table associations_otf_log;
+
+drop table disease_log;
