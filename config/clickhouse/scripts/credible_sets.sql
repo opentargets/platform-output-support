e4be54989e656325fdad605405c9ecf6ce8fcc6c
James Hayhurst <hayhurst.jd@gmail.com>
2025-11-27 17:06:50 +0000

    credsets by study and variant

--- config/clickhouse/scripts/credible_sets.sql
+++ config/clickhouse/scripts/credible_sets.sql
@@ -2,3 +2,19 @@ CREATE TABLE if not exists credible_sets engine = EmbeddedRocksDB () primary key
     select * except locus
     from credible_sets_log
 );
+
+CREATE TABLE IF NOT EXISTS credible_sets_by_study ENGINE = MergeTree ()
+ORDER BY (
+        studyType,
+        studyId,
+        studyLocusId
+    ) SETTINGS allow_nullable_key = 1 AS (
+        SELECT * except locus
+        FROM credible_sets_log
+    );
+
+CREATE TABLE IF NOT EXISTS credible_sets_by_variant ENGINE = MergeTree ()
+ORDER BY (studyType, variantId) SETTINGS allow_nullable_key = 1 AS (
+        select * except (locus, variantId), arrayJoin (locus.variantId) as variantId
+        from credible_sets_log
+    );
