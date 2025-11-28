f29cdf884665a9043bfa705dbc2b66b4b375ce48
James Hayhurst <hayhurst.jd@gmail.com>
2025-11-28 10:48:59 +0000

    set memory and threads

--- config/clickhouse/scripts/credible_sets.sql
+++ config/clickhouse/scripts/credible_sets.sql
@@ -13,6 +13,10 @@ ORDER BY (
         FROM credible_sets_log
     );
 
+SET max_memory_usage = 600000000000;
+
+SET max_threads = 4;
+
 CREATE TABLE IF NOT EXISTS credible_sets_by_variant ENGINE = MergeTree ()
 ORDER BY (studyType, variantId) SETTINGS allow_nullable_key = 1 AS (
         select * except (locus, variantId), arrayJoin (locus.variantId) as variantId
