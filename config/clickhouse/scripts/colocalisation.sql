06102df6db8ab50fbae23e3545e60efdeebefcc2
James Hayhurst <hayhurst.jd@gmail.com>
2026-01-08 09:00:34 +0000

    coloc script

--- config/clickhouse/scripts/colocalisation.sql
+++ config/clickhouse/scripts/colocalisation.sql
@@ -1,4 +1,4 @@
-create table if not exists colocalisation engine = MergeTree ()
+create table if not exists colocalisation_left engine = MergeTree ()
 order by (otherStudyType, studyLocusId) as
 select
     studyLocusId,
@@ -15,10 +15,36 @@ from (
         select
             leftStudyLocusId as studyLocusId, rightStudyLocusId as otherStudyLocusId, rightStudyType as otherStudyType, chromosome, colocalisationMethod, numberColocalisingVariants, h3, h4, clpp, betaRatioSignAverage
         from colocalisation_log
-    ), (
+    ) as left_table
+
+create table if not exists colocalisation_right engine = MergeTree ()
+order by (otherStudyType, studyLocusId) as
+select
+    studyLocusId,
+    otherStudyLocusId,
+    otherStudyType,
+    chromosome,
+    colocalisationMethod,
+    numberColocalisingVariants,
+    h3,
+    h4,
+    clpp,
+    betaRatioSignAverage
+from (
         select
             rightStudyLocusId as studyLocusId, leftStudyLocusId as otherStudyLocusId, 'gwas' as otherStudyType, chromosome, colocalisationMethod, numberColocalisingVariants, h3, h4, clpp, betaRatioSignAverage
         from colocalisation_log
+    ) as right_table;
+
+create table if not exists colocalisation engine = MergeTree ()
+order by (otherStudyType, studyLocusId) as
+select *
+from (
+        select *
+        from colocalisation_left
+        union all
+        select *
+        from colocalisation_right
     );
 
 drop table colocalisation_log;
