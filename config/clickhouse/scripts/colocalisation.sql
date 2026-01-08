ec66f04e6d23ceb39429d568b7051fc5c889b10d
James Hayhurst <hayhurst.jd@gmail.com>
2026-01-08 09:11:10 +0000

    update coloc script

--- config/clickhouse/scripts/colocalisation.sql
+++ config/clickhouse/scripts/colocalisation.sql
@@ -15,7 +15,7 @@ from (
         select
             leftStudyLocusId as studyLocusId, rightStudyLocusId as otherStudyLocusId, rightStudyType as otherStudyType, chromosome, colocalisationMethod, numberColocalisingVariants, h3, h4, clpp, betaRatioSignAverage
         from colocalisation_log
-    ) as left_table
+    ) as left_colocs;
 
 create table if not exists colocalisation_right engine = MergeTree ()
 order by (otherStudyType, studyLocusId) as
@@ -34,7 +34,7 @@ from (
         select
             rightStudyLocusId as studyLocusId, leftStudyLocusId as otherStudyLocusId, 'gwas' as otherStudyType, chromosome, colocalisationMethod, numberColocalisingVariants, h3, h4, clpp, betaRatioSignAverage
         from colocalisation_log
-    ) as right_table;
+    ) as right_colocs;
 
 create table if not exists colocalisation engine = MergeTree ()
 order by (otherStudyType, studyLocusId) as
