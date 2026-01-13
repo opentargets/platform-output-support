c06b977818789ec042781f4b31d41dbdb6f6c102
James Hayhurst <hayhurst.jd@gmail.com>
2026-01-13 16:36:48 +0000

    add disease to clickhouse

--- config/clickhouse/schema/disease.sql
+++ config/clickhouse/schema/disease.sql
@@ -1,4 +1,21 @@
 CREATE TABLE if not exists disease_log (
     `id` String,
+    `name` String,
     `therapeuticAreas` Array (LowCardinality (String)),
+    `description` Nullable (String),
+    `dbXRefs` Array (String),
+    `directLocationIds` Array (String),
+    `indirectLocationIds` Array (String),
+    `obsoleteTerms` Array (String),
+    `synonyms` Tuple (
+        `hasExactSynonym` Array (Nullable (String)),
+        `hasRelatedSynonym` Array (Nullable (String)),
+        `hasNarrowSynonym` Array (Nullable (String)),
+        `hasBroadSynonym` Array (Nullable (String))
+    ),
+    `parents` Array (LowCardinality (String)),
+    `children` Array (String),
+    `ancestors` Array (LowCardinality (String)),
+    `descendants` Array (String),
+    `ontology` Tuple (isTherapeuticArea Boolean)
 ) engine = Log;
---
+++ config/clickhouse/scripts/disease.sql
@@ -0,0 +1,57 @@
+CREATE TABLE if not exists studies_by_disease engine = MergeTree ()
+order by (diseaseId) as
+select
+    arrayJoin (diseaseIds) as diseaseId,
+    groupArrayDistinctIf (studyId, studyId != '') as studyIds
+from studies_log
+where
+    diseaseIds is not null
+group by
+    diseaseId;
+
+create table if not exists studies_by_disease_indirect engine = MergeTree ()
+order by (diseaseId) as
+SELECT
+    id as diseaseId,
+    arrayDistinct (groupArrayArray (studyIds)) AS indirectStudyIds,
+    length(indirectStudyIds) AS studyCount
+FROM (
+        SELECT
+            id,
+            arrayJoin (descendants) AS indirectDiseaseId
+        FROM disease_log
+    ) AS disease_descendants
+    LEFT JOIN studies_by_disease ON indirectDiseaseId = studies_by_disease.diseaseId
+GROUP BY
+    diseaseId;
+
+CREATE TABLE if not exists disease engine = EmbeddedRocksDB () primary key id as (
+    select
+        id,
+        name,
+        therapeuticAreas,
+        description,
+        dbXRefs,
+        directLocationIds,
+        indirectLocationIds,
+        obsoleteTerms,
+        CAST(
+            tupleToNameValuePairs (synonyms),
+            'Array(Tuple(relation String, terms Array(Nullable(String))))'
+        ) as synonyms,
+        parents,
+        children,
+        ancestors,
+        descendants,
+        ontology.isTherapeuticArea as isTherapeuticArea,
+        studies_by_disease.studyIds as studyIds,
+        indirect_studies.indirectStudyIds as indirectStudyIds
+    from
+        disease_log
+        left outer join studies_by_disease on disease_log.id = studies_by_disease.diseaseId
+        left outer join studies_by_disease_indirect as indirect_studies on disease_log.id = indirect_studies.diseaseId
+);
+
+DROP TABLE IF EXISTS studies_by_disease;
+
+DROP TABLE IF EXISTS studies_by_disease_indirect;
