1c27e4c414fbe1517c1bfda9743d6d98e5d94341
James Hayhurst <hayhurst.jd@gmail.com>
2026-01-16 16:33:03 +0000

    add credible sets to ch

--- config/clickhouse/scripts/studies.sql
+++ config/clickhouse/scripts/studies.sql
@@ -1,4 +1,17 @@
+CREATE TABLE IF NOT EXISTS credible_sets_by_study ENGINE = MergeTree
+ORDER BY (studyId) AS (
+        SELECT
+            groupArrayDistinct (studyLocusId) AS studyLocusIds, studyId
+        FROM credible_sets_log
+        GROUP BY
+            studyId
+    );
+
 CREATE TABLE if not exists studies engine = EmbeddedRocksDB () primary key studyId as (
     select *
-    from studies_log
+    from
+        studies_log
+        left outer join credible_sets_by_study on studies_log.studyId = credible_sets_by_study.studyId
 );
+
+DROP TABLE IF EXISTS credible_sets_by_study;
