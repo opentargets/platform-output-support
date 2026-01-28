2465cde345a63bab3a87d691fdda12e666b28b3f
ricardo <remo87@gmail.com>
2026-01-28 17:10:06 +0000

    add clinical_indication

---
+++ config/clickhouse/schema/clinical_indication.sql
@@ -0,0 +1,8 @@
+create table if not exists clinical_indication_log (
+    id String,
+    drugId LowCardinality (String),
+    diseaseId LowCardinality (String),
+    maxClinicalStage LowCardinality (String),
+    clinicalReportIds Array (String),
+    hasExpertReview Bool
+) engine = Log;
