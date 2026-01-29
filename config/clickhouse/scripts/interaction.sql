1306fe633942e34178f6f267a02f04ecbed4a911
James Hayhurst <hayhurst.jd@gmail.com>
2026-01-29 10:52:02 +0000

    add interaction evidence to interaction ch

--- config/clickhouse/scripts/interaction.sql
+++ config/clickhouse/scripts/interaction.sql
@@ -4,8 +4,66 @@ ORDER BY (
         sourceDatabase,
         scoring
     ) SETTINGS allow_nullable_key = 1 AS (
-        SELECT *
-        FROM interaction_log
+        SELECT i.*, groupArray (
+                (
+                    e.evidenceScore,
+                    e.expansionMethodMiIdentifier, 
+                    e.expansionMethodShortName, 
+                    e.hostOrganismScientificName, 
+                    e.hostOrganismTaxId, 
+                    e.intASource, 
+                    e.intBSource, 
+                    e.interactionDetectionMethodMiIdentifier, 
+                    e.interactionDetectionMethodShortName, 
+                    e.interactionIdentifier,
+                    e.interactionResources,
+                    e.interactionTypeMiIdentifier, 
+                    e.interactionTypeShortName, 
+                    e.participantDetectionMethodA, 
+                    e.participantDetectionMethodB, 
+                    e.pubmedId
+                )::Tuple(
+                    evidenceScore Nullable (Float64), 
+                    expansionMethodMiIdentifier Nullable (String), 
+                    expansionMethodShortName Nullable (String), 
+                    hostOrganismScientificName Nullable (String), 
+                    hostOrganismTaxId Nullable (UInt32), 
+                    intASource String, 
+                    intBSource String, 
+                    interactionDetectionMethodMiIdentifier String, 
+                    interactionDetectionMethodShortName String, 
+                    interactionIdentifier Nullable (String), 
+                    interactionResources Tuple (
+                        databaseVersion LowCardinality (String), 
+                        sourceDatabase LowCardinality (String)
+                    ),
+                    interactionTypeMiIdentifier Nullable (String), 
+                    interactionTypeShortName Nullable (String), 
+                    participantDetectionMethodA Array (Tuple (
+                        miIdentifier Nullable (String), 
+                        shortName Nullable (String)
+                        )
+                    ), 
+                    participantDetectionMethodB Array (Tuple (
+                        miIdentifier Nullable (String), 
+                        shortName Nullable (String))
+                    ), 
+                    pubmedId Nullable (String)
+                    )
+            ) AS evidences
+        FROM
+            interaction_log AS i
+            LEFT JOIN interaction_evidence_log AS e ON i.targetA = e.targetA
+            AND i.targetB = e.targetB
+            AND i.intA = e.intA
+            AND i.intB = e.intB
+            AND i.intABiologicalRole = e.intABiologicalRole
+            AND i.intBBiologicalRole = e.intBBiologicalRole
+            AND i.sourceDatabase = e.interactionResources.sourceDatabase
+        GROUP BY
+            i.*
     );
 
 DROP TABLE IF EXISTS interaction_log;
+
+DROP TABLE IF EXISTS interaction_evidence_log;
