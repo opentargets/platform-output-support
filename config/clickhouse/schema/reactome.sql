cc2f653f0a545038f63d2cad0a52a1381b405379
James Hayhurst <hayhurst.jd@gmail.com>
2026-02-10 09:46:52 +0000

    remove reactome

--- config/clickhouse/schema/reactome.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS reactome_log (
-    id String,
-    label String,
-    children Array (String),
-    parents Array (String),
-    ancestors Array (String)
-) ENGINE = Log;
