f42e1d257b59e62b964cc5e77f5ca59f96ee6bf9
James Hayhurst <hayhurst.jd@gmail.com>
2026-02-11 11:13:43 +0000

    added protein coding coords to ch

---
+++ config/clickhouse/scripts/protein_coding_coords.sql
@@ -0,0 +1,79 @@
+CREATE TABLE if not exists protein_coding_coords_by_target engine = EmbeddedRocksDB () primary key targetId as (
+    SELECT 
+        targetId,
+        groupArray(
+            (targetId,
+            uniprotAccessions,
+            aminoAcidPosition,
+            alternateAminoAcid,
+            referenceAminoAcid,
+            variantFunctionalConsequenceIds,
+            variantEffect,
+            variantId,
+            diseases,
+            datasources,
+            therapeuticAreas
+            )::Tuple(
+                targetId String,
+                uniprotAccessions Array (String),
+                aminoAcidPosition Int32,
+                alternateAminoAcid LowCardinality (String),
+                referenceAminoAcid LowCardinality (String),
+                variantFunctionalConsequenceIds Array (String),
+                variantEffect Nullable (Float32),
+                variantId String,
+                diseases Array (String),
+                datasources Array (
+                    Tuple (
+                        datasourceCount UInt32,
+                        datasourceId LowCardinality (String),
+                        datasourceNiceName LowCardinality (String)
+                    )
+                ),
+                therapeuticAreas Array (String)
+            )
+        ) as proteinCodingCoords
+    FROM protein_coding_coords_log
+    GROUP BY targetId
+);
+
+CREATE TABLE IF NOT EXISTS protein_coding_coords_by_variant ENGINE = EmbeddedRocksDB () primary key variantId as (
+    SELECT 
+        variantId,
+        groupArray(
+            (targetId,
+            uniprotAccessions,
+            aminoAcidPosition,
+            alternateAminoAcid,
+            referenceAminoAcid,
+            variantFunctionalConsequenceIds,
+            variantEffect,
+            variantId,
+            diseases,
+            datasources,
+            therapeuticAreas
+            )::Tuple(
+                targetId String,
+                uniprotAccessions Array (String),
+                aminoAcidPosition Int32,
+                alternateAminoAcid LowCardinality (String),
+                referenceAminoAcid LowCardinality (String),
+                variantFunctionalConsequenceIds Array (String),
+                variantEffect Nullable (Float32),
+                variantId String,
+                diseases Array (String),
+                datasources Array (
+                    Tuple (
+                        datasourceCount UInt32,
+                        datasourceId LowCardinality (String),
+                        datasourceNiceName LowCardinality (String)
+                    )
+                ),
+                therapeuticAreas Array (String)
+            )
+        ) as proteinCodingCoords
+    FROM protein_coding_coords_log
+    GROUP BY variantId
+);
+
+DROP TABLE IF EXISTS protein_coding_coords_log SYNC;
