593a5b7418cd734cfdbd0360760c2c6cc6502e7e
James Hayhurst <hayhurst.jd@gmail.com>
2026-02-13 15:48:39 +0000

    add mechanism of action to ch

---
+++ config/clickhouse/scripts/mechanism_of_action.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS mechanism_of_action ENGINE = EmbeddedRocksDB () PRIMARY KEY chemblId AS (
+    SELECT 
+        arrayJoin(chemblIds) AS chemblId,
+        groupArray(
+            (
+            mechanismOfAction,
+            actionType,
+            targetName,
+            targets,
+            references
+            )::Tuple(
+                mechanismOfAction String,
+                actionType Nullable (String),
+                targetName Nullable (String),
+                targets Array (String),
+                references Array (
+                    Tuple (
+                        ids Array (String),
+                        source String,
+                        urls Array (String)
+                    )
+                )
+            )
+        ) AS rows,
+        groupArrayDistinct(targetType) AS uniqueTargetTypes,
+        groupArrayDistinct(actionType) AS uniqueActionTypes
+    FROM mechanism_of_action_log
+    GROUP BY
+        chemblId
+);
+
+DROP TABLE IF EXISTS mechanism_of_action_log SYNC;
