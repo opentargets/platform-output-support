49877d334232eb0a2aaa489da04bbbef8af2f788
ricardo <remo87@gmail.com>
2026-03-03 15:57:52 +0000

    update config drug and evidence schema

--- config/clickhouse/schema/drug.sql
+++ config/clickhouse/schema/drug.sql
@@ -4,9 +4,7 @@ CREATE TABLE IF NOT EXISTS drug_log (
     synonyms Array (String),
     tradeNames Array (String),
     childChemblIds Array (String),
-    yearOfFirstApproval Nullable (Int32),
     drugType String,
-    isApproved Nullable (Bool),
     crossReferences Array (
         Tuple (
             source String,
@@ -14,16 +12,6 @@ CREATE TABLE IF NOT EXISTS drug_log (
         )
     ),
     parentId Nullable (String),
-    maximumClinicalTrialPhase Nullable (Float32),
-    hasBeenWithdrawn Bool,
-    linkedDiseases Tuple (
-        count Nullable (Int32),
-        rows Array (String)
-    ),
-    linkedTargets Tuple (
-        count Nullable (Int32),
-        rows Array (String)
-    ),
-    blackBoxWarning Bool,
+    maximumClinicalStage String,
     description Nullable (String)
 ) ENGINE = Log;
