3b32bdfd8ddc350e3bd06f8c2699941bd7155aa1
James Hayhurst <hayhurst.jd@gmail.com>
2026-03-03 10:47:11 +0000

    remove second literature table
    removed day from literature table

--- config/clickhouse/scripts/literature.sql
+++ config/clickhouse/scripts/literature.sql
@@ -1,19 +1,10 @@
-create table if not exists literature_index engine = MergeTree ()
-order by (
-        keywordId, pmid, year, month, day
-    ) as (
-        select
-            pmid, pmcid, keywordId, relevance, date, year, month, day
-        from literature_log
+CREATE TABLE IF NOT EXISTS literature ENGINE = MergeTree ()
+ORDER BY (
+        keywordId, year, month, relevance
+    ) AS (
+        SELECT
+            pmid, pmcid, keywordId, relevance, date, year, month
+        FROM literature_log
     );
 
-create table if not exists literature engine = MergeTree ()
-order by (pmid, year) as (
-        select
-            pmid, any (pmcid) as pmcid, any (date) as date, any (year) as year, any (month) as month, any (day) as day
-        from literature_log
-        group by
-            pmid
-    );
-
-drop table literature_log SYNC;
+DROP TABLE literature_log SYNC;
